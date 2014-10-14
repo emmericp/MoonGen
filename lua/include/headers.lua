15832b78e4340fafcfcbc3d4ccb07eb91440a45a
scholzd <scholzd@in.tum.de>
2014-10-14 15:15:12 +0200

    formatting + packed

--- lua/include/headers.lua
+++ lua/include/headers.lua
@@ -2,17 +2,17 @@ local ffi = require "ffi"
 
 -- structs
 ffi.cdef[[
-	struct mac_address {
+	struct __attribute__ ((__packed__)) mac_address {
 		uint8_t byte[6];
 	};
 
-	struct ethernet_header {
+	struct __attribute__ ((__packed__)) ethernet_header {
 		struct mac_address 	dst;
 		struct mac_address 	src;
 		uint16_t 		ethertype;
 	};
 
-	struct ipv4_address {
+	struct __attribute__ ((__packed__)) ipv4_address {
 		union {
 			uint8_t		byte[4];
 			uint16_t	word[2];
@@ -20,49 +20,48 @@ ffi.cdef[[
 		};
 	};
 
-	struct ipv6_address {
+	struct __attribute__ ((__packed__)) ipv6_address {
 		union {
 			uint8_t 	byte[16];
 			uint16_t	word[8];
 		};
 	};
 
-	struct ipv4_header {
-		 uint8_t	verihl; 		                  /* hardcoded version + ihl!! */ 
-		 uint8_t      	tos;                    /* type of service */                   
-		 uint16_t     	len;                    /* total length */              
-		 uint16_t     	id;                     /* identification */                    
-		 uint16_t     	fragOff;                /* fragment offset field */     
-		 uint8_t      	ttl;                    /* time to live */                      
-		 uint8_t      	protocol;               /* protocol */                  
-		 uint16_t     	check;                  /* checksum */       
-		 
-		 struct ipv4_address	src;
-		 struct ipv4_address	dst;
+	struct __attribute__ ((__packed__)) ipv4_header {
+		uint8_t	verihl; 			/* hardcoded version + ihl!! */ 
+		uint8_t      	tos;                    /* type of service */                   
+		uint16_t     	len;                    /* total length */              
+		uint16_t     	id;                     /* identification */                    
+		uint16_t     	fragOff;                /* fragment offset field */     
+		uint8_t      	ttl;                    /* time to live */                      
+		uint8_t      	protocol;               /* protocol */                  
+		uint16_t     	check;                  /* checksum */       
+	
+		struct ipv4_address	src;		/* source address */
+		struct ipv4_address	dst;		/* destination address */
 	 };
 
-	 struct ipv6_header {
-		uint32_t 	vtf; 			  /* = version(6) + Traffic Class(0) + Flow Label(0); */    
-		                 			/* should be 0x6000 0000 on our architecture */           
-					                                                                                  
-	  uint16_t  len;                                     
-		uint8_t   nexthdr;                                        
-		uint8_t   ttl;                                       
+	struct __attribute__ ((__packed__)) ipv6_header {
+		uint32_t 	vtf; 			/* = version(6) + Traffic Class(0) + Flow Label(0); */    
+							/* should be 0x6000 0000 on our architecture */           
+		uint16_t  	len;			/* payload length */
+		uint8_t   	nexthdr;		/* next header */
+		uint8_t   	ttl;  			/* time to live */                                 
 		                                                               
-		struct ipv6_address src;
-		struct ipv6_address	dst;
+		struct ipv6_address 	src;		/* source address */
+		struct ipv6_address	dst;		/* destination address */
 	};
 
-	 struct udp_header {                                                             
-		 uint16_t     src;	    /* source port */                               
-		 uint16_t     dst;    	/* destination port */                          
-		 uint16_t     len;    	/* udp length */                                
-		 uint16_t     check; 	  /* udp checksum */                              
+	struct __attribute__ ((__packed__)) udp_header {                                                             
+		uint16_t 	src;	    		/* source port */                               
+		uint16_t     	dst;    		/* destination port */                          
+		uint16_t     	len;    		/* udp length */                                
+		uint16_t     	check; 	  		/* udp checksum */                              
 	};
 
 	struct __attribute__ ((__packed__)) packet {
 		struct ethernet_header  eth_h;
-		union __attribute__ ((__packed__)) {
+		union {
 			struct ipv4_header 	ipv4_h;
 		//	struct ipv6_header	ipv6_h;
 		};
