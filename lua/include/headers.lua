06656a99567845bbacb83ccd70a73d9ea53c1d21
Paul Emmerich <emmericp@net.in.tum.de>
2014-10-21 15:12:29 +0200

    add C structs for headers

---
+++ lua/include/headers.lua
@@ -0,0 +1,71 @@
+local ffi = require "ffi"
+
+-- structs
+ffi.cdef[[
+	// TODO: vlan support (which can be offloaded to the NIC to simplify scripts)
+	struct __attribute__((__packed__)) ethernet_header {
+		uint8_t		dst[6];
+		uint8_t		src[6];
+		uint16_t	type;
+	};
+
+	struct __attribute__((__packed__)) ipv4_address {
+		union {
+			uint8_t		uint8[4];
+			uint32_t	uint32;
+		};
+	};
+
+	struct __attribute__((__packed__)) ipv6_address {
+		union {
+			uint8_t 	uint8[16];
+			uint32_t	uint32[4];
+			uint64_t	uint32[2];
+		};
+	};
+
+	struct __attribute__((__packed__)) ipv4_header {
+		uint8_t			verihl;
+		uint8_t			tos;
+		uint16_t		len;
+		uint16_t		id;
+		uint16_t		frag;
+		uint8_t			ttl;
+		uint8_t			protocol;
+		uint16_t		cs;
+		struct ipv4_address	src;
+		struct ipv4_address	dst;
+	 };
+
+	struct __attribute__((__packed__)) ipv6_header {
+		uint32_t 		vtf;
+		uint16_t  		len;
+		uint8_t   		nextHeader;
+		uint8_t   		ttl;
+		struct ipv6_address 	src;
+		struct ipv6_address	dst;
+	};
+
+	struct __attribute__((__packed__)) udp_header {                                                             
+		uint16_t 	src;
+		uint16_t     	dst;
+		uint16_t     	len;
+		uint16_t     	cs;
+	};
+
+	struct __attribute__((__packed__)) udp_packet {
+		struct ethernet_header  eth;
+		struct ipv4_header 	ip;
+		struct udp_header 	udp;
+		uint8_t			payload[];
+	};
+
+	struct __attribute__((__packed__)) udp_v6_packet {
+		struct ethernet_header  eth;
+		struct ipv6_header 	ip;
+		struct udp_header 	udp;
+		uint8_t			payload[];
+	};
+]]
+
+return ffi.C
