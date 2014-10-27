fe528daa187b4d835210fecf0d5b72a4fc21731f
Lukas Erlacher <erlacher@in.tum.de>
2014-10-27 23:15:07 +0100

    Add histogram class, test in l2-load-latency example

---
+++ lua/include/histogram.lua
@@ -0,0 +1,29 @@
+histogram = {}
+histogram.__index = histogram
+
+function histogram.create()
+	local histo = {}
+	setmetatable(histo, histogram)
+	histo.histo = {}
+	return histo
+end
+
+function histogram:update(k)
+	self.histo[k] = (self.histo[k] or 0) +1
+end
+
+function histogram:sort()
+	self.sortedHisto = {}
+	self.sum = 0
+	self.samples = 0
+
+	for k, v in pairs(self.histo) do
+		table.insert(self.sortedHisto, { k = k, v = v})
+		self.samples = self.samples + v
+		self.sum = self.sum + k * v
+	end
+	table.sort(self.sortedHisto, function(e1, e2) return e1.k < e2.k end)
+	return self.sortedHisto
+end
+
+return histogram
