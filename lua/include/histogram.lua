aedb4cb5591b136c464bfdb420425d681a785147
Lukas Erlacher <l.erlacher@gmail.com>
2014-10-29 11:49:41 +0100

    add stat calculations to histogram

--- lua/include/histogram.lua
+++ lua/include/histogram.lua
@@ -2,28 +2,53 @@ histogram = {}
 histogram.__index = histogram
 
 function histogram.create()
-	local histo = {}
-	setmetatable(histo, histogram)
-	histo.histo = {}
-	return histo
+    local histo = {}
+    setmetatable(histo, histogram)
+    histo.histo = {}
+    return histo
 end
 
 function histogram:update(k)
-	self.histo[k] = (self.histo[k] or 0) +1
+    self.histo[k] = (self.histo[k] or 0) +1
 end
 
-function histogram:sort()
-	self.sortedHisto = {}
-	self.sum = 0
-	self.samples = 0
+function histogram:calc()
+    self.sortedHisto = {}
+    self.sum = 0
+    self.samples = 0
 
-	for k, v in pairs(self.histo) do
-		table.insert(self.sortedHisto, { k = k, v = v})
-		self.samples = self.samples + v
-		self.sum = self.sum + k * v
-	end
-	table.sort(self.sortedHisto, function(e1, e2) return e1.k < e2.k end)
-	return self.sortedHisto
+    for k, v in pairs(self.histo) do
+        table.insert(self.sortedHisto, { k = k, v = v})
+        self.samples = self.samples + v
+        self.sum = self.sum + k * v
+    end
+    self.avg = self.sum / self.samples
+    table.sort(self.sortedHisto, function(e1, e2) return e1.k < e2.k end)
+    return self.sortedHisto
+end
+
+function histogram:stat()
+        if not self.sortedHisto then self:calc() end
+
+        local quart_samples = self.samples / 4
+
+        self.lower_quart = nil
+        self.median = nil
+        self.upper_quart = nil
+
+        local idx = 0
+        for dummy, p in pairs(self.sortedHisto) do
+                if not self.lower_quart and idx >= quart_samples then
+                        self.lower_quart = p.k
+                elseif not self.median and idx >= quart_samples * 2 then
+                        self.median = p.k
+                elseif not self.upper_quart and idx >= quart_samples * 3 then
+                        self.upper_quart = p.k
+                        break
+                end
+
+                idx = idx + p.v
+        end
 end
 
 return histogram
