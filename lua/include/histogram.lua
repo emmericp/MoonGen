29bbf35617f47be96cffae097b7083b588815ab5
Lukas Erlacher <l.erlacher@gmail.com>
2014-10-29 18:28:44 +0100

    add iterator

--- lua/include/histogram.lua
+++ lua/include/histogram.lua
@@ -2,53 +2,68 @@ local histogram = {}
 histogram.__index = histogram
 
 function histogram.create()
-    local histo = {}
-    setmetatable(histo, histogram)
-    histo.histo = {}
-    return histo
+	local histo = {}
+	setmetatable(histo, histogram)
+	histo.histo = {}
+	histo.dirty = true
+	return histo
 end
 
 function histogram:update(k)
-    self.histo[k] = (self.histo[k] or 0) +1
+	self.histo[k] = (self.histo[k] or 0) +1
+	self.dirty = true
 end
 
 function histogram:calc()
-    self.sortedHisto = {}
-    self.sum = 0
-    self.samples = 0
+	self.sortedHisto = {}
+	self.sum = 0
+	self.samples = 0
 
-    for k, v in pairs(self.histo) do
-        table.insert(self.sortedHisto, { k = k, v = v})
-        self.samples = self.samples + v
-        self.sum = self.sum + k * v
-    end
-    self.avg = self.sum / self.samples
-    table.sort(self.sortedHisto, function(e1, e2) return e1.k < e2.k end)
-    return self.sortedHisto
+	for k, v in pairs(self.histo) do
+		table.insert(self.sortedHisto, { k = k, v = v})
+		self.samples = self.samples + v
+		self.sum = self.sum + k * v
+	end
+	self.avg = self.sum / self.samples
+	table.sort(self.sortedHisto, function(e1, e2) return e1.k < e2.k end)
+	self.dirty = false
+	return self.sortedHisto
 end
 
 function histogram:stat()
-        if not self.sortedHisto then self:calc() end
+		if self.dirty then self:calc() end
 
-        local quart_samples = self.samples / 4
+		local quartSamples = self.samples / 4
 
-        self.lower_quart = nil
-        self.median = nil
-        self.upper_quart = nil
+		self.lowerQuart = nil
+		self.median = nil
+		self.upperQuart = nil
 
-        local idx = 0
-        for dummy, p in pairs(self.sortedHisto) do
-                if not self.lower_quart and idx >= quart_samples then
-                        self.lower_quart = p.k
-                elseif not self.median and idx >= quart_samples * 2 then
-                        self.median = p.k
-                elseif not self.upper_quart and idx >= quart_samples * 3 then
-                        self.upper_quart = p.k
-                        break
-                end
+		local idx = 0
+		for _, p in ipairs(self.sortedHisto) do
+				if not self.lowerQuart and idx >= quartSamples then
+						self.lowerQuart = p.k
+				elseif not self.median and idx >= quartSamples * 2 then
+						self.median = p.k
+				elseif not self.upperQuart and idx >= quartSamples * 3 then
+						self.upperQuart = p.k
+						break
+				end
 
-                idx = idx + p.v
-        end
+				idx = idx + p.v
+		end
+end
+
+function histogram:samples()
+	local i = 0
+	if self.dirty then self:calc() end
+	local n = #(self.sortedHisto)
+	return function ()
+		if not self.dirty then
+			i = i+1
+			if i <= n then return self.sortedHisto[i] end
+		end
+	end
 end
 
 return histogram
