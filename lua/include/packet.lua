d8b12462a4630860f86ebe5fb92b7d071c4b352d
scholzd <scholzd@in.tum.de>
2014-11-05 15:40:00 +0100

    fixed mistakes; ipv6 functions just dummies yet

--- lua/include/packet.lua
+++ lua/include/packet.lua
@@ -39,7 +39,7 @@ local ip4Header = {}
 ip4Header.__index = ip4Header
 
 function ip4Header:calculateChecksum()
-	self.cs = 0 --reset as packets can be reused
+	self.cs = 0 --just to be sure...
 	self.cs = checksum(self, 20)
 end
 
@@ -56,17 +56,25 @@ end
 
 function parseIPAddress(ip)
 	local bytes = {}
-	bytes = insertInTable(string.match(ip, '(%d+).(%d+).(%d+).(%d+)'))
+	bytes = {string.match(ip, '(%d+)%.(%d+)%.(%d+)%.(%d+)')}
+	if bytes == nil then
+		return
+	end
 	for i = 1, 4 do
-		if bytes[i] == nil or bytes[i] < 0 or bytes[i] > 255 then
+		if bytes[i] == nil then
 			return 
 		end
+		bytes[i] = tonumber(bytes[i])
+		if  bytes[i] < 0 or bytes[i] > 255 then
+			return
+		end
 	end
-	bytes = tonumberall(bytes)
 	
 	ip = bytes[1]
 	for i = 2, 4 do
-		ip = bor(lshift(tmp, 8), bytes[i])
+		ip = bor(lshift(ip, 8), bytes[i])
+	
+	end
 	return  ip 
 end
 
@@ -89,34 +97,49 @@ function ip6Addr:get()
 end
 
 function ip6Addr:set(ip)
-	--TODO
+	--TODO 
 end
 
 function parseIP6Address(ip)
 	local bytes = {}
-	-- TODO: better parsing
-	bytes = insertInTable(string.match(maxIp, '(%x%x)(%x%x):(%x%x)(%x%x):(%x%x)(%x%x):(%x%x)(%x%x):(%x%x)(%x%x):(%x%x)(%x%x):(%x%x)(%x%x):(%x%x)(%x%x)'))
+	-- TODO: better parsing (shortened addresses)
+	bytes = {string.match(ip, '(%x%x)(%x%x):(%x%x)(%x%x):(%x%x)(%x%x):(%x%x)(%x%x):(%x%x)(%x%x):(%x%x)(%x%x):(%x%x)(%x%x):(%x%x)(%x%x)')}
 
+	if bytes == nil then
+		return
+	end
 	for i = 1, 16 do
 		if bytes[i] == nil then
 			return 
 		end
+		bytes[i] = tonumber(bytes[i], 16)
 	end
-	bytes = tonumberall(bytes, 16)
+	--bytes = tonumberall(bytes, 16)
 	
 	--TODO 
 end
 
 function ip6Addr:getString()
 	return ("%x%x:%x%x:%x%x:%x%x:%x%x:%x%x:%x%x:%x%x"):format(self.uint8[0], self.uint8[1], self.uint8[2], self.uint8[3], 
-															  self.uint8[4], self.uint8[5], self.uint8[6], self.uint8[7], 
-															  self.uint8[8], self.uint8[9], self.uint8[10], self.uint8[11], 
-															  self.uint8[12], self.uint8[13], self.uint8[14], self.uint8[15])
+								  self.uint8[4], self.uint8[5], self.uint8[6], self.uint8[7], 
+								  self.uint8[8], self.uint8[9], self.uint8[10], self.uint8[11], 
+								  self.uint8[12], self.uint8[13], self.uint8[14], self.uint8[15])
+end
+
+-- udp
+
+local udpHeader = {}
+udpHeader.__index = udpHeader
+
+function udpHeader:calculateChecksum()
+	-- TODO as it is mandatory for IPv6
+	self.cs = 0
 end
 
 ffi.metatype("struct ipv4_header", ip4Header)
 ffi.metatype("union ipv4_address", ip4Addr)
 ffi.metatype("union ipv6_address", ip6Addr)
+ffi.metatype("struct udp_header", udpHeader)
 ffi.metatype("struct rte_mbuf", pkt)
 
 
