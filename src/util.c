91d40d446a1040ada66f71474ac92403df7f3af8
Paul Emmerich <emmericp@net.in.tum.de>
2014-11-30 20:45:20 +0100

    speed up udp pseudo header checksum calculation
    vectorizing the code is not worth it due to alignment requirements

--- src/util.c
+++ src/util.c
@@ -21,44 +21,7 @@ uint64_t rte_rdtsc() {
 }
 
 
-// copied from dpdk test-pmd
-static inline uint16_t
-get_16b_sum(uint16_t *ptr16, uint32_t nr)
-{
-	uint32_t sum = 0;
-	while (nr > 1)
-	{
-		sum +=*ptr16;
-		nr -= sizeof(uint16_t);
-		ptr16++;
-		if (sum > UINT16_MAX)
-			sum -= UINT16_MAX;
-	}
-
-	return (uint16_t)sum;
-}
-
-
-// copied from dpdk test-pmd
-static inline uint16_t
-get_ipv4_psd_sum (struct ipv4_hdr * ip_hdr)
-{
-	/* Pseudo Header for IPv4/UDP/TCP checksum */
-	union ipv4_psd_header {
-		struct {
-			uint32_t src_addr; /* IP address of source host. */
-			uint32_t dst_addr; /* IP address of destination host(s). */
-			uint8_t  zero;     /* zero. */
-			uint8_t  proto;    /* L4 protocol type. */
-			uint16_t len;      /* L4 length. */
-		} __attribute__((__packed__));
-		uint16_t u16_arr[0];
-	} psd_hdr;
-
-	psd_hdr.src_addr = ip_hdr->src_addr;
-	psd_hdr.dst_addr = ip_hdr->dst_addr;
-	psd_hdr.zero     = 0;
-	psd_hdr.proto    = ip_hdr->next_proto_id;
+static inline uint16_t get_ipv4_psd_sum (struct ipv4_hdr* ip_hdr) {
 	uint16_t len = ip_hdr->total_length;
 	// TODO: depends on CPU endianess
 	// and yes, this optimization is actually worth it:
@@ -66,12 +29,21 @@ get_ipv4_psd_sum (struct ipv4_hdr * ip_hdr)
 	//	* 1.2% in l3-multi-flows.lua 
 	if (len & 0xFF) { // lower (network byte order) byte used --> len >= 256
 		// just use swap
-		psd_hdr.len = rte_bswap16((uint16_t)(rte_bswap16(len) - sizeof(struct ipv4_hdr)));
+		len = rte_bswap16((uint16_t)(rte_bswap16(len) - sizeof(struct ipv4_hdr)));
 	} else {
 		// can use shift instead, yeah.
-		psd_hdr.len = ((len >> 8) - sizeof(struct ipv4_hdr)) << 8;
+		len = ((len >> 8) - sizeof(struct ipv4_hdr)) << 8;
 	}
-	return get_16b_sum(psd_hdr.u16_arr, sizeof(psd_hdr));
+	uint64_t sum = (uint64_t) ip_hdr->src_addr + (uint64_t) ip_hdr->dst_addr + (uint64_t) ((ip_hdr->next_proto_id << 24) | len);
+	uint32_t lower = sum & 0xFFFFFFFF;
+	uint32_t upper = sum >> 32;
+	lower += upper;
+	if (lower < upper) lower++;
+	uint16_t lower16 = lower & 0xFFFF;
+	uint16_t upper16 = lower >> 16;
+	lower16 += upper16;
+	if (lower16 < upper16) lower16++;
+	return lower16;
 }
 
 // TODO: cope with flexible offsets
