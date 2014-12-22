48a39455667426d810bda478d788b38745090aa1
Paul Emmerich <emmericp@net.in.tum.de>
2014-12-22 17:06:40 +0100

    fix hugetlbfs mount check

--- setup-hugetlbfs.sh
+++ setup-hugetlbfs.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 mkdir -p /mnt/huge
-(mount > /dev/null | grep hugetlbfs) || mount -t hugetlbfs nodev /mnt/huge
+(mount | grep hugetlbfs) > /dev/null || mount -t hugetlbfs nodev /mnt/huge
 echo 512 > /sys/devices/system/node/node0/hugepages/hugepages-2048kB/nr_hugepages
 
