816a64edb666583ad16f4549e4183d81b7ab1992
Paul Emmerich <emmericp@net.in.tum.de>
2015-01-04 23:31:51 +0100

    update l3 timestamping to 'new' API

--- examples/l3-load-latency.lua
+++ examples/l3-load-latency.lua
@@ -1,98 +1,69 @@
-local dpdk	= require "dpdk"
+local dpdk		= require "dpdk"
 local memory	= require "memory"
-local dev	= require "device"
-local ts	= require "timestamping"
-local dpdkc	= require "dpdkc"
+local device	= require "device"
+local ts		= require "timestamping"
+local dpdkc		= require "dpdkc"
 local filter	= require "filter"
-
-local ffi	= require "ffi"
-
--- NOTE: this code was written against the old API of MoonGen and does not work anymore
--- TODO: add support for L3 filtering, the idea is to match the PTP type field in hardware (possible with flow director on intel cards)
+local ffi		= require "ffi"
 
 function master(...)
 	local txPort, rxPort, rate, size = tonumberall(...)
 	if not txPort or not rxPort then
-		errorf("usage: txPort rxPort")
+		errorf("usage: txPort rxPort [rate [size]]")
 	end
-	rate = rate or 1
-	size = size or 128
-	local rxMempool = memory.createMemPool(2047)
+	rate = rate or 2000
+	size = size or 124
+	local rxMempool = memory.createMemPool()
 	if txPort == rxPort then
-		dev.config(txPort, rxMempool, 2, 2)
+		txDev = device.config(txPort, rxMempool, 1, 2)
+		rxDev = txDev
+		txDev:wait()
 	else
-		dev.config(txPort, rxMempool, 1, 2)
-		dev.config(rxPort, rxMempool, 2, 1)
+		txDev = device.config(txPort, rxMempool, 1, 2)
+		rxDev = device.config(rxPort, rxMempool, 2, 1)
+		device.waitForDevs(txDev, rxDev)
 	end
-	dev.waitForPorts(txPort, rxPort)
-	dev.setTxRate(txPort, 0, rate)
-	dpdkc.rte_eth_promiscuous_disable(rxPort)
-	dpdk.setRuntime(20)
-	dpdk.launchLua("timerSlave", txPort, rxPort, 1, 0)
-	dpdk.launchLua("loadSlave", txPort, 0, size)
-	dpdk.launchLua("counterSlave", rxPort, 0, size)
+	txDev:getTxQueue(1):setRate(rate)
+	dpdk.launchLua("timerSlave", txPort, rxPort, 0, 0, size)
+	dpdk.launchLua("loadSlave", txPort, 1, size, 4)
+	dpdk.launchLua("counterSlave", rxPort, size)
 	dpdk.waitForSlaves()
 end
 
-function loadSlave(port, queue, size)
-	local NUM_BUFS = 1023
-	local mempool = memory.createMemPool(NUM_BUFS)
-	local bufs = {}
-	for i = 1, NUM_BUFS do
-		local buf = memory.alloc(mempool)
-		ts.fillPacket(buf, 1234) 
+function loadSlave(port, queue, size, numFlows)
+	local queue = device.get(port):getTxQueue(queue)
+	local mempool = memory.createMemPool(function(buf)
+		ts.fillPacket(buf, 319) 
 		local data = ffi.cast("uint8_t*", buf.pkt.data)
-		-- dst mac
-		data[0] = 0x00
-		data[1] = 0x11
-		data[2] = 0x22
-		data[3] = 0x33
-		data[4] = 0x44
-		data[5] = 0xff
-		-- src mac
-		for i = 6, 11 do
-			data[i] = i
-		end
 		data[43] = 0x00 -- PTP version, set to 0 to disable timestamping for load packets
-		data[58] = 0x00 -- timestamp indicator
-		data[59] = 0x00
-		bufs[#bufs + 1] = buf
-	end
-	for i, v in ipairs(bufs) do
-		dpdkc.rte_pktmbuf_free_export(v)
-	end
-	local MAX_BURST_SIZE = 31
-	local bufs = ffi.new("struct rte_mbuf*[?]", MAX_BURST_SIZE)
+	end)
 	local lastPrint = dpdk.getTime()
 	local totalSent = 0
 	local lastTotal = 0
 	local lastSent = 0
-	local lastTime = dpdk.getTime()
+	local bufs = mempool:bufArray(128)
+	local baseIP = 0x01020304 -- TODO: ip.parse("1.2.3.4")
+	local counter = 0
 	while dpdk.running() do
-		local burstSize = MAX_BURST_SIZE
-		for i = 0, MAX_BURST_SIZE - 1 do
-			-- this should be hidden behind some API...
-			local buf = memory.alloc(mempool)
-			buf.pkt.pkt_len = size - 4
-			buf.pkt.data_len = size - 4
-			if  buf == nil then
-				i = i - 1
+		bufs:fill(size)
+		for i, buf in ipairs(bufs) do
+			local pkt = buf:getUDPPacket()
+			pkt.ip.src:set(baseIP + counter)
+			if numFlows <= 32 then
+				-- this is significantly faster for small numbers
+				-- TODO: this optimization shouldn't be necessary...
+				counter = (counter + 1) % numFlows
 			else
-				bufs[i] = buf
+				counter = counter == numFlows and 0 or counter + 1
 			end
 		end
-		local sent = 0
-		while true do
-			sent = sent + dpdkc.rte_eth_tx_burst_export(port, queue, bufs + sent, MAX_BURST_SIZE - sent)
-			if sent >= MAX_BURST_SIZE then
-				break
-			end
-		end
-		totalSent = totalSent + sent
+		-- UDP checksums are optional, so using just IPv4 checksums would be sufficient here
+		bufs:offloadUdpChecksums()
+		totalSent = totalSent + queue:send(bufs)
 		local time = dpdk.getTime()
 		if time - lastPrint > 1 then
 			local mpps = (totalSent - lastTotal) / (time - lastPrint) / 10^6
-			printf("Sent %d packets, current rate %.2f Mpps, %.2f MBit/s", totalSent, mpps, mpps * (size + 20) * 8)
+			printf("Sent %d packets, current rate %.2f Mpps, %.2f MBit/s, %.2f MBit/s wire rate", totalSent, mpps, mpps * (size + 4) * 8, mpps * (size + 24) * 8)
 			lastTotal = totalSent
 			lastPrint = time
 		end
@@ -101,13 +72,15 @@ function loadSlave(port, queue, size)
 end
 
 function counterSlave(port)
-	--filter.l2Filter(port, 0x0800, 63) -- routings packets to a queue that doesn't exist drops them
+	local dev = device.get(port)
+	--dev:l2Filter(0x1234, filter.DROP)
+	--TODO: implement fdir flex byte filters
 	local total = 0
 	while dpdk.running() do
 		local time = dpdk.getTime()
-		dpdkc.rte_delay_ms_export(1000)
+		dpdk.sleepMillis(1000)
 		local elapsed = dpdk.getTime() - time
-		local pkts = dev.getRxStats(port)
+		local pkts = dev:getRxStats(port)
 		total = total + pkts
 		printf("Received %d packets, current rate %.2f Mpps", total, pkts / elapsed / 10^6)
 	end
@@ -115,67 +88,45 @@ function counterSlave(port)
 end
 
 function timerSlave(txPort, rxPort, txQueue, rxQueue, size)
-	local bufs = ffi.new("struct rte_mbuf*[?]", 1)
-	local rxBufs = ffi.new("struct rte_mbuf*[?]", 32)
-	ts.enableTimestamps(txPort, 0) -- TODO: split enableTimestamps into rx and tx
-	ts.enableTimestamps(rxPort, rxQueue, 1234)
+	local txDev = device.get(txPort)
+	local rxDev = device.get(rxPort)
+	local txQueue = txDev:getTxQueue(txQueue)
+	local rxQueue = rxDev:getRxQueue(rxQueue)
+	local mem = memory.createMemPool()
+	local bufs = mem:bufArray(1)
+	local rxBufs = mem:bufArray(128)
+	txQueue:enableTimestamps()
+	rxQueue:enableTimestamps(319)
 	local hist = {}
-	local mempool = memory.createMemPool(1024)
-	local lastSent = 0
+	-- wait one second, otherwise we might start timestamping before the load is applied
+	dpdk.sleepMillis(1000)
 	while dpdk.running() do
-		local tx
-		-- send with simple rate control
-		local sent = false
-		if dpdk.getTime() - lastSent >= 0.002 then -- max rate: about 500 packets/s
-			lastSent = dpdk.getTime()
-			sent = true
-			bufs[0] = memory.alloc(mempool)
-			ts.fillPacket(bufs[0], 1234, size)
-			local data = ffi.cast("uint8_t*", bufs[0].pkt.data)
-			data[0] = 0x00
-			data[1] = 0x11
-			data[2] = 0x22
-			data[3] = 0x33
-			data[4] = 0x44
-			data[5] = 0xff
-			ts.syncClocks(txPort, rxPort)
-			while dpdkc.rte_eth_tx_burst_export(txPort, txQueue, bufs, 1) == 0 do end
-			for i = 1, 100 do
-				tx = ts.tryReadTxTimestamp(txPort)
-				if tx then
-					break
+		bufs:fill(size - 4)
+		ts.fillPacket(bufs[1], 319, size)
+		-- sync clocks and send
+		ts.syncClocks(txDev, rxDev)
+		txQueue:send(bufs)
+		-- increment the wait time when using large packets or slower links
+		local tx = txQueue:getTimestamp(100)
+		if tx then
+			dpdk.sleepMicros(500) -- minimum latency to limit the packet rate
+			-- sent was successful, try to get the packet back (max. 10 ms wait time before we assume the packet is lost)
+			local rx = rxQueue:tryRecv(rxBufs, 10000)
+			if rx > 0 then
+				local numPkts = 0
+				for i = 1, rx do
+					if bit.bor(rxBufs[i].ol_flags, dpdk.PKT_RX_IEEE1588_TMST) ~= 0 then
+						numPkts = numPkts + 1
+					end
 				end
-				dpdkc.rte_delay_us_export(10)
-			end
-		end
-		-- receive
-		-- TODO: dynamically adjust the max wait time
-		local tries = 3000 -- wait for max 3 ms, assume as lost otherwise
-		while tries > 0 do
-			local recv
-			repeat
-				recv = dpdkc.rte_eth_rx_burst_export(rxPort, rxQueue, rxBufs, 32) 
-			until recv ~= 0 or not dpdk.running()
-			local found = false
-			for i = 0, recv - 1 do
-				local data = ffi.cast("uint8_t*", rxBufs[i].pkt.data)
-				if data[58] == 0x54 then -- TS packet, yay
-					found = true
-				end
-				dpdkc.rte_pktmbuf_free_export(rxBufs[i])
-			end
-			if found or not sent then
-				break
-			end
-			if recv < 20 then
-				tries = tries - 1
-				dpdkc.rte_delay_us_export(1);
-			end
-		end
-		if tries > 0 and tx and sent then
-			local delay = (ts.readRxTimestamp(rxPort) - tx) * 6.4
-			if delay > 0 and delay < 100000000 then
-				hist[delay] = (hist[delay] or 0) + 1
+				local delay = (rxQueue:getTimestamp() - tx) * 6.4
+				if numPkts == 1 then
+					if delay > 0 and delay < 100000000 then
+						hist[delay] = (hist[delay] or 0) + 1
+					end
+				end -- else: got more than one packet, so we got a problem
+				-- TODO: use sequence numbers in the packets to avoid bugs here
+				rxBufs:freeAll()
 			end
 		end
 	end
@@ -192,17 +143,8 @@ function timerSlave(txPort, rxPort, txQueue, rxQueue, size)
 		samples = samples + v.v
 		print(v.k, v.v)
 	end
-	local avg = sum / samples
-	-- calc stddev
-	-- TODO: move to a separate function
-	local stddev = 0
-	for k, v in pairs(hist) do
-		stddev = stddev + (k - avg)^2 * v
-	end
-	local stddev = math.sqrt(stddev / samples)
 	print()
-	print("Average: " .. avg .. " ns, stddev: " .. stddev .. " ns, samples: " .. samples)
+	print("Average: " .. (sum / samples) .. " ns, " .. samples .. " samples")
 	print("----------------------------------------------")
-	io.stdout:flush()
 end
 
