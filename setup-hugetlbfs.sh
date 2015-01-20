d55a9fc698d5c88eda3f1776ec5b89db0b4f9bcb
Paul Emmerich <emmericp@net.in.tum.de>
2015-01-20 23:37:04 +0100

    add multi-socket support

--- setup-hugetlbfs.sh
+++ setup-hugetlbfs.sh
@@ -1,5 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 mkdir -p /mnt/huge
 (mount | grep hugetlbfs) > /dev/null || mount -t hugetlbfs nodev /mnt/huge
-echo 512 > /sys/devices/system/node/node0/hugepages/hugepages-2048kB/nr_hugepages
+for i in {0..7}
+do
+	if [[ -e "/sys/devices/system/node/node$i" ]]
+	then
+		echo 512 > /sys/devices/system/node/node$i/hugepages/hugepages-2048kB/nr_hugepages
+	fi
+done
 
