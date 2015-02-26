4002346c2a0f9754c485c54452f92939068d7386
Paul Emmerich <emmericp@net.in.tum.de>
2015-02-26 01:31:39 +0100

    fix histograms with few bins

--- lua/include/histogram.lua
+++ lua/include/histogram.lua
@@ -2,12 +2,12 @@ local histogram = {}
 histogram.__index = histogram
 
 function histogram.create()
-	local histo = {}
-	setmetatable(histo, histogram)
+	local histo = setmetatable({}, histogram)
 	histo.histo = {}
 	histo.dirty = true
 	return histo
 end
+setmetatable(histogram, { __call = histogram.create })
 
 function histogram:update(k)
 	self.histo[k] = (self.histo[k] or 0) +1
@@ -20,13 +20,21 @@ function histogram:calc()
 	self.numSamples = 0
 
 	for k, v in pairs(self.histo) do
-		table.insert(self.sortedHisto, {k = k, v = v})
+		table.insert(self.sortedHisto, { k = k, v = v })
 		self.numSamples = self.numSamples + v
 		self.sum = self.sum + k * v
 	end
 	self.avg = self.sum / self.numSamples
+	local stdDevSum = 0
+	for k, v in pairs(self.histo) do
+		stdDevSum = stdDevSum + v * (k - self.avg)^2
+	end
+	self.stdDev = (stdDevSum / (self.numSamples - 1)) ^ 0.5
+
 	table.sort(self.sortedHisto, function(e1, e2) return e1.k < e2.k end)
 	
+	-- TODO: this is obviously not entirely correct for numbers not divisible by 4
+	-- however, it doesn't really matter for the number of samples we usually use
 	local quartSamples = self.numSamples / 4
 
 	self.lowerQuart = nil
@@ -35,15 +43,18 @@ function histogram:calc()
 
 	local idx = 0
 	for _, p in ipairs(self.sortedHisto) do
-		if not self.lowerQuart and idx >= quartSamples then
-			self.lowerQuart = p.k
-		elseif not self.median and idx >= quartSamples * 2 then
-			self.median = p.k
-		elseif not self.upperQuart and idx >= quartSamples * 3 then
-			self.upperQuart = p.k
-			break
+		-- TODO: inefficient
+		for _ = 1, p.v do
+			if not self.lowerQuart and idx >= quartSamples then
+				self.lowerQuart = p.k
+			elseif not self.median and idx >= quartSamples * 2 then
+				self.median = p.k
+			elseif not self.upperQuart and idx >= quartSamples * 3 then
+				self.upperQuart = p.k
+				break
+			end
+			idx = idx + 1
 		end
-		idx = idx + p.v
 	end
 	self.dirty = false
 end
@@ -52,6 +63,18 @@ function histogram:totals()
 	if self.dirty then self:calc() end
 
 	return self.numSamples, self.sum, self.avg
+end
+
+function histogram:avg()
+	if self.dirty then self:calc() end
+
+	return self.avg
+end
+
+function histogram:standardDeviation()
+	if self.dirty then self:calc() end
+
+	return self.stdDev
 end
 
 function histogram:quartiles()
