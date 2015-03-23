f0eb55ef2e6cb87461237df38954131522041284
Paul Emmerich <emmericp@net.in.tum.de>
2015-03-23 01:51:06 +0100

    add reset() and wait() methods for timers

--- lua/include/timer.lua
+++ lua/include/timer.lua
@@ -7,6 +7,7 @@ timer.__index = timer
 
 function mod:new(time)
 	return setmetatable({
+		time = time,
 		stop = dpdk.getTime() + time
 	}, timer)
 end
@@ -18,5 +19,24 @@ end
 function timer:expired()
 	return self.stop <= dpdk.getTime()
 end
+
+function timer:reset()
+	self.stop = dpdk.getTime() + self.time
+end
+
+--- Perform a busy wait on the timer.
+-- Returns early if MoonGen is stopped (mg.running() == false).
+function timer:busyWait()
+	while not self:expired() and dpdk.running() do
+	end
+	return dpdk.running()
+end
+
+--- Perform a non-busy wait on the timer.
+-- Might be less accurate than busyWait()
+function timer:wait()
+	error("NYI")
+end
+
 return mod
 
