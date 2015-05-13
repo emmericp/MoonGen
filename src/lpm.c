1ff209fc1bcf31ab7c487523b02f3eb6d8e075e7
Rainer <rainer@wwad.de>
2015-05-13 15:18:09 +0200

    lpm with debugging output

--- src/lpm.c
+++ src/lpm.c
@@ -13,7 +13,14 @@
  *
  * Documentation for the respective functions can be found in rte_table.h
  ***/
+void printhex(void* data, int len){
+  int i;
+  for(i=0;i<len;i++){
+    printf("%2x ", ((uint8_t*)data)[i]);
+  }
+  printf("\n");
 
+}
 void* mg_lpm_table_create(void *params, int socket_id, uint32_t entry_size)
 {
   return rte_table_lpm_ops.f_create(params, socket_id, entry_size);
@@ -30,6 +37,10 @@ int mg_lpm_table_entry_add(
 	int *key_found,
 	void **entry_ptr)
 {
+  printf("add key: ");
+  printhex(key, 5);
+  printf("add entry: ");
+  printhex(entry, 5);
   return rte_table_lpm_ops.f_add(table, key, entry, key_found, entry_ptr);
 }
 
@@ -49,7 +60,22 @@ int mg_lpm_table_lookup(
 	uint64_t *lookup_hit_mask,
 	void **entries)
 {
-  return rte_table_lpm_ops.f_lookup(table, pkts, pkts_mask, lookup_hit_mask, entries);
+  struct rte_pktmbuf pkt0 = pkts[0]->pkt;
+  void * data = pkt0.data;
+  int i;
+  for(i=0;i<4;i++){
+    printf("%2x ", ((uint8_t*)data)[i]);
+  }
+  printf("\n");
+
+  // FIXME: XXX: pkts_mask hardcoded to 1 for debugging
+  int result = rte_table_lpm_ops.f_lookup(table, pkts, 1, lookup_hit_mask, entries);
+  printf("hit mask c : ");
+  printhex(lookup_hit_mask, 8);
+  printf("C result entry: ");
+  printhex(entries[0], 5);
+
+  return result;
 }
 
 
