6e4a6fc5196564533158d25783e9cbd028af907c
Paul Emmerich <emmericp@net.in.tum.de>
2015-05-17 00:54:34 +0200

    build: check if dpdk submodule is initialized

--- build.sh
+++ build.sh
@@ -13,7 +13,14 @@ then
 fi
 make -j 8 'CFLAGS=-DLUAJIT_NUMMODE=2 -DLUAJIT_ENABLE_LUA52COMPAT'
 make install DESTDIR=$(pwd)
+
 cd ../dpdk
+if [[ ! -e Makefile ]]
+then
+	echo "ERROR: DPDK submodule not initialized"
+	echo "Please run git submodule update --init"
+	exit 1
+fi
 make -j 8 install T=x86_64-native-linuxapp-gcc
 ../../bind-interfaces.sh
 cd ../../build
