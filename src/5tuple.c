13aeb4bc63935941d1626f2ac3357e3fb311cb5b
Rainer <rainer@wwad.de>
2015-05-25 17:49:26 +0200

    5tuple burst classification code

--- src/5tuple.c
+++ src/5tuple.c
@@ -1,5 +1,9 @@
 #include "5tuple.h"
 
+#define OFF_ETHHEAD	(sizeof(struct ether_hdr))
+#define OFF_IPV42PROTO (offsetof(struct ipv4_hdr, next_proto_id))
+#define MBUF_IPV4_2PROTO(m)	\
+	(rte_pktmbuf_mtod((m), uint8_t *) + OFF_ETHHEAD + OFF_IPV42PROTO)
 
 enum {
 	PROTO_FIELD_IPV4,
@@ -108,11 +112,63 @@ int mg_5tuple_build_filter(struct rte_acl_ctx * acx, uint32_t num_categories){
 int mg_5tuple_classify_burst(
     struct rte_acl_ctx * acx,
     struct rte_mbuf **pkts,
-    uint16_t n,
     struct mg_bitmask* pkts_mask,
+    uint32_t categories,
+    struct mg_bitmask** result_masks,
+    uint32_t ** result_entries
     //FIXME: what will be the result?
     ){
 
+  uint16_t i;
+  uint8_t * data[n];
 
+  // compress:
+  uint16_t n_real=0;
+  for(i=0;i<pkts_mask->size;i++){
+    if(mg_bitmask_get_bit(pkts_mask, i)){
+      uint8_t* data[n_real] = MBUF_IPV4_2PROTO(pkt);
+      n_real++;
+    }
+  }
+
+  // compute results:
+  uint32_t results[num_categories * n_real];
+  rte_acl_classify(acx, data, results, n_real, categories);
+
+  // decompress:
+  //uint32_t c;
+  //for(c=0; c< categories; c++){
+  //  n_real = 0;
+  //  for(i=0;i<pkts_mask.size;i++){
+  //    if(mg_bitmask_get_bit(pkts_mask, i)){
+  //      uint32_t result = results[n_real*categories + c];
+  //      result_entries[c][i] = result;
+  //      if(result){
+  //        mg_bitmask_set_bit(result_masks[c], i);
+  //      }
+  //      n_real++;
+  //    }
+  //  }
+  //}
+  
+
+  // decompress:
+  uint32_t category = 0;
+  uint16_t packet = 0;
+  for(i= 0; i< num_categories*n_real; i++){
+    if(category == num_categories){
+      category = 0;
+      packet++;
+      while( mg_bitmask_get_bit(pkts_mask, packet) == 0){
+        packet++;
+      }
+    }
+    if(results[i]){
+      mg_bitmask_set_bit(result_masks[category], packet);
+    }
+    result_entries[category][packet] = results[i];
+    category++;
+  }
+  return 0;
 }
 
