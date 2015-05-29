5df90b296507ebdb55fa4d1f67d111eb4dbcf3dc
Rainer <rainer@wwad.de>
2015-05-29 16:49:00 +0200

    distribute compiles

--- src/distribute.c
+++ src/distribute.c
@@ -2,8 +2,13 @@
 
 #include <rte_cycles.h>
 #include <rte_ethdev.h>
+#include "rte_malloc.h"
 
-struct mg_distribute_config * mg_distribute_create(uint16_t entry_offset, uint16_t nr_outputs, uint8_t always_flush){
+struct mg_distribute_config * mg_distribute_create(
+  uint16_t entry_offset,
+  uint16_t nr_outputs,
+  uint8_t always_flush
+  ){
   struct mg_distribute_config *cfg = rte_zmalloc(NULL, sizeof(struct mg_distribute_config) + nr_outputs * sizeof(struct mg_distribute_output), 0);
   if(cfg){
     cfg->nr_outputs = nr_outputs;
@@ -18,8 +23,8 @@ int mg_distribute_output_flush(
   uint16_t number
   ){
   struct mg_distribute_queue * queue = cfg->outputs[number].queue;
-  if(queue->nex_idx == 0){
-    printf(" output %d should have been flushed, but was empty!\n", output);
+  if(queue->next_idx == 0){
+    printf(" output %d should have been flushed, but was empty!\n", number);
     return 0;
   }
   // Busy wait, until all packets are stored in tx descriptors.
@@ -28,7 +33,7 @@ int mg_distribute_output_flush(
     uint16_t transmitted = rte_eth_tx_burst (cfg->outputs[number].port_id, cfg->outputs[number].queue_id, queue->pkts, queue->next_idx);
     queue->next_idx -= transmitted;
   }
-  printf(" output %d has been flushed!\n", output);
+  printf(" output %d has been flushed!\n", number);
   return 0;
 }
 
@@ -40,15 +45,14 @@ int mg_distribute_register_output(
   uint8_t port_id,
   uint16_t queue_id,
   uint16_t burst_size,
-  uint64_t timeout,
+  uint64_t timeout
   ){
   if(number >= cfg->nr_outputs){
     printf("ERROR: invalid outputnumber\n");
     return -EINVAL;
   }
   cfg->outputs[number].port_id = port_id; 
-  cfg->outputs[number].queue_id = quque_id; 
-  cfg->outputs[number].burst_size = burst_size; 
+  cfg->outputs[number].queue_id = queue_id; 
   cfg->outputs[number].timeout = timeout; 
   cfg->outputs[number].valid = 1; 
   if(burst_size != 0){
@@ -57,6 +61,7 @@ int mg_distribute_register_output(
     // FIXME: MACRO for cacheline size?
     struct mg_distribute_queue *queue = rte_zmalloc(NULL, sizeof(struct mg_distribute_queue) + burst_size * sizeof(struct rte_mbuf*), 64);
     cfg->outputs[number].queue = queue;
+    cfg->outputs[number].queue->size = burst_size; 
   }
   return 0;
 }
@@ -73,11 +78,11 @@ int mg_distribute_send(
   //  - we always iterate multiple of 64...
   //    -> maybe save cycles, when burst is not multiple of 64?
   int i;
-  for(i = pkts_mask->n_blocks; i>0; i--);
+  for(i = 0; i < pkts_mask->n_blocks; i++){
     uint64_t mask = 1ULL;
     while(mask){
       mask = mask<<1;
-      if(mask & pkts_mask->mask[n]){
+      if(mask & pkts_mask->mask[i]){
         // determine output, to send the packet to
         uint8_t output = ((uint8_t*)(*entries))[cfg->entry_offset];
         // send pkt to the corresponding output...
@@ -103,7 +108,8 @@ int mg_distribute_send(
     //FIXME check if output valid
     for (i = 0; i < cfg->nr_outputs; i++){
       if(likely(cfg->outputs[i].valid)){
-      mg_distribute_output_flush(cfg, i);
+        mg_distribute_output_flush(cfg, i);
+      }
     }
   }
   return 0;
@@ -124,7 +130,7 @@ int mg_distribute_send(
 // Call frequency of this function defines the resolution of timeouts for
 // queue flushes.
 void mg_distribute_handle_timeouts(
-  struct mg_distribute_config *cfg,
+  struct mg_distribute_config *cfg
   ){
   int i;
   uint64_t time = rte_rdtsc();
--- src/distribute.h
+++ src/distribute.h
@@ -7,15 +7,34 @@
 #include <rte_mbuf.h>
 #include "bitmask.h"
 
+
+
 struct mg_distribute_queue{
   uint16_t next_idx;
   uint16_t size;
   struct rte_mbuf *pkts[0];
-}
+};
+
+struct mg_distribute_output{
+  uint8_t valid;
+  uint8_t port_id;
+  uint16_t queue_id;
+  uint64_t timeout;
+  uint64_t time_first_added;
+  struct mg_distribute_queue *queue;
+};
+
+struct mg_distribute_config{
+  uint16_t entry_offset;
+  uint16_t nr_outputs;
+  uint8_t always_flush;
+  struct mg_distribute_output outputs[0];
+};
+
 
 inline int8_t mg_distribute_enqueue(
   struct mg_distribute_queue * queue,
-  struct rte_mbuf *pkt,
+  struct rte_mbuf *pkt
   ){
   queue->pkts[queue->next_idx] = pkt;
   queue->next_idx++;
@@ -31,21 +50,35 @@ inline int8_t mg_distribute_enqueue(
   return 0;
 }
 
-struct mg_distribute_output{
-  uint8_t valid;
-  uint8_t port_id;
-  uint16_t queue_id;
-  uint64_t timeout;
-  uint64_t time_first_added;
-  struct mg_distribute_queue *queue;
-}
+struct mg_distribute_config * mg_distribute_create(
+    uint16_t entry_offset,
+    uint16_t nr_outputs,
+    uint8_t always_flush
+    );
 
-struct mg_distribute_config{
-  uint16_t entry_offset;
-  uint16_t nr_outputs;
-  uint8_t always_flush;
-  struct mg_distribute_output outputs[0];
-}
+int mg_distribute_output_flush(
+  struct mg_distribute_config *cfg,
+  uint16_t number
+  );
 
+int mg_distribute_register_output(
+  struct mg_distribute_config *cfg,
+  uint16_t number,
+  uint8_t port_id,
+  uint16_t queue_id,
+  uint16_t burst_size,
+  uint64_t timeout
+  );
+
+int mg_distribute_send(
+  struct mg_distribute_config *cfg,
+  struct rte_mbuf **pkts,
+  struct mg_bitmask* pkts_mask,
+  void **entries
+  );
+
+void mg_distribute_handle_timeouts(
+  struct mg_distribute_config *cfg
+  );
 
 #endif
