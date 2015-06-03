7234606fb51354556af6e5e6541ce9a5c0aba2fc
Lukas Märdian <maerdian@in.tum.de>
2015-06-03 15:45:41 +0200

    ipsec: send & recv 10 pkts, try to decrypt

--- examples/ipsec-vpn.lua
+++ examples/ipsec-vpn.lua
@@ -35,72 +35,71 @@ function txSlave(port, srcQueue, dstQueue)
 			ip4Protocol = 0x32, --ESP, 0x33=AH
 			ip4Src = "10.0.0.1",
 			ip4Dst = "192.168.1.1",
-			espSPI = 0x01020304,
-			espSQN = 0xbbbbbbbb,
+			espSPI = 0xdeadbeef,
+			espSQN = 0,
 			espIV  = iv,
 		}
 	end)
-	bufs = mem:bufArray(128) --Array of 128 pkts
+	bufs = mem:bufArray(10) --Array of 10 pkts
 
-	--SA_IDX 42 is hard coded in TX context descriptor
-	ipsec.tx_set_key(port, 42, "77777777deadbeef77777777DEADBEEF", "ff0000ff")
-	local key, salt = ipsec.tx_get_key(port, 42)
-	print("Key:  0x"..key)
-	print("Salt: 0x"..salt)
+	ipsec.tx_set_key(port, 0, "77777777deadbeef77777777DEADBEEF", "ff0000ff")
+	--local key, salt = ipsec.tx_get_key(port, 42)
+	--print("Key:  0x"..key)
+	--print("Salt: 0x"..salt)
 
-	while dpdk.running() do
+	--while dpdk.running() do
 		bufs:alloc(74)
 		for _, buf in ipairs(bufs) do
 			local pkt = buf:getEspPacket()
-			pkt.payload.uint8[0] = 0xee -- real payload
-			--pkt.payload.uint32[0] = 0xeeeeeeee -- real payload
-			ipsec.add_esp_trailer(buf, 1) -- add 20 byte ESP trailer
+			pkt.payload.uint8[0] = 0xaa -- real payload
+			pkt.payload.uint8[1] = 0xbb -- real payload
+			pkt.payload.uint8[2] = 0xcc -- real payload
+			ipsec.add_esp_trailer(buf, 3) -- add 20 byte ESP trailer
 			count = (count+1) % 0xffffffff
 		end
 		bufs:offloadIPChecksums()
-		bufs:offloadIPSec(42, "esp", 1)
+		--bufs:offloadIPSec(0, "esp", 1)
 		srcQueue:send(bufs)
-	end
+	--end
 	ipsec.disable(port)
 end
 
 -- rxSlave logs received packages
 function rxSlave(port, queue)
-	ipsec.rx_set_key(port, 0, "ffffffffdeadbeef77777777DEADBEEF", "ff0420ff")
-	local key, salt, valid, proto, decrypt, ipv6 = ipsec.rx_get_key(port, 0)
-	print("Key:  0x"..key)
-	print("Salt: 0x"..salt)
-	print("Valid ("..valid.."), Proto ("..proto.."), Decrypt ("..decrypt.."), IPv6 ("..ipv6..")")
+	ipsec.enable(port)
+	ipsec.rx_set_key(port, 0, "77777777deadbeef77777777DEADBEEF", "ff0000ff")
+	--ipsec.rx_set_key(port, 0, "ffffffffdeadbeef77777777DEADBEEF", "ff0420ff")
+	--local key, salt, valid, proto, decrypt, ipv6 = ipsec.rx_get_key(port, 0)
+	--print("Key:  0x"..key)
+	--print("Salt: 0x"..salt)
+	--print("Valid ("..valid.."), Proto ("..proto.."), Decrypt ("..decrypt.."), IPv6 ("..ipv6..")")
 
-	ipsec.rx_set_ip(port, 0, "1.2.3.4")
-	local ip  = ipsec.rx_get_ip(port, 0)
-	print("IP: "..ip)
-	ipsec.rx_set_ip(port, 1, "0123:4567:89AB:CDEF:1011:1213:1415:1617")
-	local ip = ipsec.rx_get_ip(port, 1, false)
-	print("IP: "..ip)
+	ipsec.rx_set_ip(port, 0, "192.168.1.1")
+	--local ip  = ipsec.rx_get_ip(port, 0)
+	--print("IP: "..ip)
+	--ipsec.rx_set_ip(port, 1, "0123:4567:89AB:CDEF:1011:1213:1415:1617")
+	--local ip = ipsec.rx_get_ip(port, 1, false)
+	--print("IP: "..ip)
 
 	ipsec.rx_set_spi(port, 0, 0xdeadbeef, 0)
-	local spi, ip_idx = ipsec.rx_get_spi(port, 0)
-	print("SPI:    0x"..bit.tohex(spi, 8))
-	print("IP_IDX: "..ip_idx)
+	--local spi, ip_idx = ipsec.rx_get_spi(port, 0)
+	--print("SPI:    0x"..bit.tohex(spi, 8))
+	--print("IP_IDX: "..ip_idx)
 
 	local dev = device.get(port)
 	local bufs = memory.bufArray()
 	local total = 0
-	while dpdk.running() do
+	while total < 10 do
 		local rx = queue:recv(bufs)
-		--for i = 1, rx do
-		--	local buf  = bufs[i]
-		--	local pkt = buf:getEspPacket()
-		--	printf("C: %u", pkt.payload.uint32[1])
-		--end
-		-- Dump only one packet per second
-		local buf = bufs[rx]
-		local pkt = buf:getEspPacket()
-		buf:dump(128) -- hexdump of received packet (incl. header)
-		printf("H: 0x%x", pkt.payload.uint32[0])
-		printf("C: 0x%x (%u)", pkt.payload.uint32[1], pkt.payload.uint32[1])
-		printf("T: 0x%x", pkt.payload.uint32[2])
+		for i = 1, rx do
+			local buf  = bufs[i]
+			local pkt = buf:getEspPacket()
+			buf:dump(128) -- hexdump of received packet (incl. header)
+			printf("uint8[0]: %x", pkt.payload.uint8[0])
+			printf("uint8[1]: %x", pkt.payload.uint8[1])
+			printf("uint8[2]: %x", pkt.payload.uint8[2])
+			printf("uint8[3]: %x", pkt.payload.uint8[3])
+		end
 		bufs:freeAll()
 
 		local time = dpdk.getTime()
@@ -111,5 +110,6 @@ function rxSlave(port, queue)
 		printf("Received %d packets, current rate %.2f Mpps\n", total, pkts / elapsed / 10^6)
 	end
 	print("Received "..total.." packets")
+	ipsec.disable(port)
 end
 
