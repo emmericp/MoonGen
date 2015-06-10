7b092b8ac19ce5bcd8ba743743fd3d8b900b2b52
Lukas Märdian <luk@slyon.de>
2015-06-10 17:04:44 +0200

    ipsec: fix rte_mbuf assert: sizeof(struct rte_mbuf) is now two cache lines (i.e. 128 bytes)

--- deps/dpdk/app/test/test_mbuf.c
+++ deps/dpdk/app/test/test_mbuf.c
@@ -832,8 +832,7 @@ test_failing_mbuf_sanity_check(void)
 static int
 test_mbuf(void)
 {
-	//FIXME: TODO: make this test work again (incl. ipsec data in mbuf struct)
-	//RTE_BUILD_BUG_ON(sizeof(struct rte_mbuf) != 64);
+	RTE_BUILD_BUG_ON(sizeof(struct rte_mbuf) != 128);
 
 	/* create pktmbuf pool if it does not exist */
 	if (pktmbuf_pool == NULL) {
