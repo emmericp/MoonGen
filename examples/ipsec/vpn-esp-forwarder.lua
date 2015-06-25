5d07db021a77534f96e9d4138014f565f381aabf
Lukas Märdian <maerdian@in.tum.de>
2015-06-25 17:28:05 +0200

    ipsec: vpn-esp encapsulator (WIP: check ESP padding)

--- examples/ipsec/vpn-esp-forwarder.lua
+++ examples/ipsec/vpn-esp-forwarder.lua
@@ -13,10 +13,99 @@ function master(txPort, rxPort)
 	local rxDev = device.config(rxPort, 1)
 	device.waitForLinks()
 
-	dpdk.launchLua("encryptSlave", txPort, rxPort, txDev:getTxQueue(0), rxDev:getRxQueue(0))
+	dpdk.launchLua("dumpSlave", rxPort, rxDev:getRxQueue(0), rxDev)
+	--dpdk.launchLua("encryptSlave", txPort, rxPort, txDev:getTxQueue(0), rxDev:getRxQueue(0))
 	-- TODO: add decryptSlave, for the other VPN-End-Point
+	dpdk.launchLua("loadSlave", txPort, txDev:getTxQueue(0), txDev, 256)
 
 	dpdk.waitForSlaves()
+end
+
+function loadSlave(port, queue, dev, numFlows)
+	local mem = memory.createMemPool(function(buf)
+		buf:getUdpPacket():fill{
+			pktLength = 60,
+			ethSrc = queue,
+			ethDst = "10:11:12:13:14:15",
+			ip4Dst = "192.168.1.1",
+			udpSrc = 1234,
+			udpDst = 5678,
+		}
+	end)
+	bufs = mem:bufArray(128)
+	local baseIP = parseIPAddress("10.0.0.1")
+	local flow = 0
+	local ctr = stats:newDevTxCounter(dev, "plain")
+	while dpdk.running() do
+		bufs:alloc(60)
+		for _, buf in ipairs(bufs) do
+			local pkt = buf:getUdpPacket()
+			pkt.ip4.src:set(baseIP + flow)
+			flow = incAndWrap(flow, numFlows)
+		end
+		-- UDP checksums are optional, so just IP checksums are sufficient here
+		bufs:offloadIPChecksums()
+		queue:send(bufs)
+		ctr:update()
+	end
+	ctr:finalize()
+end
+
+function dumpSlave(port, queue, dev)
+	local rxCtr = stats:newDevRxCounter(dev, "plain")
+	local iv = ffi.new("union ipsec_iv")
+	iv.uint32[0] = 0x01020304
+	iv.uint32[1] = 0x05060708
+	local new_mem = memory.createMemPool(function(buf)
+		buf:getEspPacket():fill{
+			--pktLength = new_len,
+			ethSrc = queue,
+			ethDst = "a0:36:9f:3b:71:da", --dstQueue,
+			ip4Protocol = 0x32, --ESP
+			ip4Src = "192.168.1.1",
+			ip4Dst = "192.168.1.2",
+			espSPI = 0xdeadbeef,
+			espSQN = 0,
+			espIV  = iv,
+		}
+	end)
+	local new_bufs = new_mem:bufArray(1) -- allocate one ESP packet
+
+	local bufs = memory.bufArray()
+	while dpdk.running() do
+		local rx = queue:recv(bufs)
+		for i = 1, rx do
+			local buf = bufs[i]
+			print("Original Pkt:")
+			buf:dump(128)
+			local pkt = buf:getIPPacket()
+			local eth_pkt = buf:getEthPacket()
+			local len = pkt.ip4:getLength()
+
+			local new_len = 14+20+16+len+20 -- eth(14), ip4(20), esp(16), __pkt__(len), esp_trailer(20)
+			new_bufs:alloc(new_len)
+			for _, new_buf in ipairs(new_bufs) do --FIXME: there is actually only one buf/pkt in new_bufs
+				local new_pkt = new_buf:getEspPacket()
+				new_pkt:setLength(new_len)
+				-- copy old pkt (starting with IP header) into new ESP pkt
+				for i = 0, len-1 do
+					new_pkt.payload.uint8[i] = eth_pkt.payload.uint8[i]
+				end
+				print("New Pkt:")
+				new_buf:dump(128)
+				ipsec.add_esp_trailer(new_buf, len, 0x4) -- Tunnel mode: next_header = 0x4 (IPv4)
+				print("New Pkt (with ESP Trailer):")
+				new_buf:dump(128)
+			end
+			--TODO:
+			--new_bufs:offloadIPChecksums()
+			--new_bufs:offloadIPSec(0, "esp", 1)
+			--queue:send(new_bufs)
+		end
+		bufs:freeAll()
+		rxCtr:update()
+	end
+	rxCtr:finalize()
 end
 
 -- encryptSlave encapsulates and encrypts and forwards incoming packets with IPSec/ESP (AES128-GCM)
