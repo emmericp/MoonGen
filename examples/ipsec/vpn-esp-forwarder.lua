aab386afb7a4b48d3042fadca26b9488f30d2ca7
Lukas Märdian <maerdian@in.tum.de>
2015-06-30 13:31:45 +0200

    ipsec: vpn: introduce vpn_encapsulate function

--- examples/ipsec/vpn-esp-forwarder.lua
+++ examples/ipsec/vpn-esp-forwarder.lua
@@ -4,6 +4,8 @@ local memory	= require "memory"
 local device	= require "device"
 local ffi	= require "ffi"
 local stats	= require "stats"
+local math	= require "math"
+local ip	= require "proto.ip4"
 
 function master(txPort, rxPort)
 	if not txPort or not rxPort then
@@ -21,13 +23,104 @@ function master(txPort, rxPort)
 	dpdk.waitForSlaves()
 end
 
+function vpn_decapsulate(buf, src_mac, dst_mac)
+	local new_mem = memory.createMemPool(function(buf)
+		buf:getEthPacket():fill{
+			--pktLength = new_len,
+			ethSrc = src_mac,
+			ethDst = dst_mac,
+		}
+	end)
+	local new_bufs = new_mem:bufArray(1) -- allocate one ETH packet
+
+	print("Original (ESP) Pkt:")
+	buf:dump()
+	local pkt = buf:getIPPacket()
+	local esp_pkt = buf:getEspPacket()
+
+	local len = pkt.ip4:getLength()
+	local extra_pad = pkt.payload.uint8[len-16-1-1] --ICV(16), next_hdr(1), array_starts_at_0(1)
+	print("Extra pad: " .. extra_pad)
+	-- eth(14), pkt(len), pad(extra_pad), outer_ip(20), esp_header(16), esp_trailer(20)
+	local new_len = 14+len-extra_pad-20-16-20
+
+	new_bufs:alloc(new_len)
+	local new_buf = new_bufs[1]
+	local new_pkt = new_buf:getEthPacket()
+	new_pkt:setLength(new_len)
+
+	-- copy old pkt (starting with IP header) into new ETH pkt
+	for i = 0, new_len-14-1 do
+		new_pkt.payload.uint8[i] = esp_pkt.payload.uint8[i]
+	end
+
+	print("New Pkt:")
+	new_buf:dump()
+
+	--queue:send(new_bufs)
+	--new_bufs:freeAll() --discard all generated pkts (so it wont segfault)
+	return new_bufs
+end
+
+function vpn_encapsulate(buf, spi, sa_idx, src_mac, src_ip, dst_mac, dst_ip)
+	local iv = ffi.new("union ipsec_iv")
+	iv.uint32[0] = math.random(0, 2^32-1)
+	iv.uint32[1] = math.random(0, 2^32-1)
+	local new_mem = memory.createMemPool(function(buf)
+		buf:getEspPacket():fill{
+			--pktLength = new_len,
+			ethSrc = src_mac,
+			ethDst = dst_mac,
+			ip4Protocol = 0x32, --ESP
+			ip4Src = src_ip,
+			ip4Dst = dst_ip,
+			espSPI = spi,
+			espSQN = 0,
+			espIV  = iv,
+		}
+	end)
+	local new_bufs = new_mem:bufArray(1) -- allocate one ESP packet
+
+	print("Original Pkt:")
+	buf:dump()
+	local pkt = buf:getIPPacket()
+	local eth_pkt = buf:getEthPacket()
+
+	local len = pkt.ip4:getLength()
+	local extra_pad = ipsec.calc_extra_pad(len) --for 4 byte alignment
+	-- eth(14), ip4(20), esp(16), pkt(len), pad(extra_pad), esp_trailer(20)
+	local new_len = 14+20+16+len+extra_pad+20
+
+	new_bufs:alloc(new_len)
+	local new_buf = new_bufs[1]
+	local new_pkt = new_buf:getEspPacket()
+	new_pkt:setLength(new_len)
+
+	-- copy old pkt (starting with IP header) into new ESP pkt
+	for i = 0, len-1 do
+		new_pkt.payload.uint8[i] = eth_pkt.payload.uint8[i]
+	end
+
+	ipsec.add_esp_trailer(new_buf, len, 0x4) -- Tunnel mode: next_header = 0x4 (IPv4)
+
+	print("New Pkt (with ESP Trailer):")
+	new_buf:dump()
+
+	new_bufs:offloadIPChecksums()
+	new_bufs:offloadIPSec(sa_idx, "esp", 1)
+
+	--queue:send(new_bufs)
+	--new_bufs:freeAll() --discard all generated pkts (so it wont segfault)
+	return new_bufs
+end
+
 function loadSlave(port, queue, dev, numFlows)
 	local mem = memory.createMemPool(function(buf)
 		buf:getUdpPacket():fill{
 			pktLength = 60,
 			ethSrc = queue,
 			ethDst = "10:11:12:13:14:15",
-			ip4Dst = "192.168.1.1",
+			ip4Dst = "10.0.1.1",
 			udpSrc = 1234,
 			udpDst = 5678,
 		}
@@ -53,121 +146,30 @@ end
 
 function dumpSlave(port, queue, dev)
 	local rxCtr = stats:newDevRxCounter(dev, "plain")
-	local iv = ffi.new("union ipsec_iv")
-	iv.uint32[0] = 0x01020304
-	iv.uint32[1] = 0x05060708
-	local new_mem = memory.createMemPool(function(buf)
-		buf:getEspPacket():fill{
-			--pktLength = new_len,
-			ethSrc = queue,
-			ethDst = "a0:36:9f:3b:71:da", --dstQueue,
-			ip4Protocol = 0x32, --ESP
-			ip4Src = "192.168.1.1",
-			ip4Dst = "192.168.1.2",
-			espSPI = 0xdeadbeef,
-			espSQN = 0,
-			espIV  = iv,
-		}
-	end)
-	local new_bufs = new_mem:bufArray(1) -- allocate one ESP packet
 
 	local bufs = memory.bufArray()
 	while dpdk.running() do
 		local rx = queue:recv(bufs)
+		--encapsulate all received packets
 		for i = 1, rx do
 			local buf = bufs[i]
-			--print("Original Pkt:")
-			--buf:dump(128)
 			local pkt = buf:getIPPacket()
-			local eth_pkt = buf:getEthPacket()
-			local len = pkt.ip4:getLength()
 
-			local extra_pad = ipsec.calc_extra_pad(len) --for 4 byte alignment
-			local new_len = 14+20+16+len+extra_pad+20 -- eth(14), ip4(20), esp(16), pkt(len), pad(extra_pad), esp_trailer(20)
-			new_bufs:alloc(new_len)
-			for _, new_buf in ipairs(new_bufs) do --FIXME: there is actually only one buf/pkt in new_bufs
-				local new_pkt = new_buf:getEspPacket()
-				new_pkt:setLength(new_len)
-				-- copy old pkt (starting with IP header) into new ESP pkt
-				for i = 0, len-1 do
-					new_pkt.payload.uint8[i] = eth_pkt.payload.uint8[i]
-				end
-				--print("New Pkt:")
-				--new_buf:dump(128)
-				ipsec.add_esp_trailer(new_buf, len, 0x4) -- Tunnel mode: next_header = 0x4 (IPv4)
-				--print("New Pkt (with ESP Trailer):")
-				--new_buf:dump(128)
+			if pkt.ip4:getProtocol() == ip.PROTO_ESP then
+				--TODO: Send to destination network (from VPN tunnel)
+				--TODO: vpn_decapsulate
+			else
+				local encapsulated_bufs = vpn_encapsulate(
+					buf, 0xdeadbeef, 0,
+					queue, "192.168.1.1", "a0:36:9f:3b:71:da", "192.168.1.2")
+
+				--TODO: Send to VPN tunnel (from destination network)
+				--txQueue:send(encapsulated_bufs)
+				encapsulated_bufs:freeAll() --discard all generated pkts (so it wont segfault)
 			end
-			--TODO:
-			--new_bufs:offloadIPChecksums()
-			--new_bufs:offloadIPSec(0, "esp", 1)
-			--queue:send(new_bufs)
-			new_bufs:freeAll() --discard all generated pkts (so it wont segfault)
 		end
 		bufs:freeAll()
 		rxCtr:update()
 	end
 	rxCtr:finalize()
 end
-
--- encryptSlave encapsulates and encrypts and forwards incoming packets with IPSec/ESP (AES128-GCM)
-function encryptSlave(txPort, rxPort, txQueue, rxQueue)
-	-- Enable hw crypto engine
-	ipsec.enable(txPort)
-
-	-- Install TX Security Association (SA), here only Key and Salt.
-	-- SPI, Mode and Type are set in the packet
-	ipsec.tx_set_key(txPort, 0, "77777777deadbeef77777777DEADBEEF", "ff0000ff")
-
-	-- Prepare Initialization Vector for the packet
-	-- FIXME: This should be random and unique
-	local iv = ffi.new("union ipsec_iv")
-	iv.uint32[0] = 0x01020304
-	iv.uint32[1] = 0x05060708
-
-	-- Create a packet Blueprint
-	local pkt_len = 86 -- for ESP the packet must be 4 bytes aligned
-	local mem = memory.createMemPool(function(buf)
-		buf:getEspPacket():fill{
-			pktLength = pkt_len,
-			ethSrc = srcQueue,
-			ethDst = "a0:36:9f:3b:71:da", --dstQueue,
-			ip4Protocol = 0x32, --ESP
-			ip4Src = "192.168.1.1",
-			ip4Dst = "192.168.1.2",
-			espSPI = 0xdeadbeef,
-			espSQN = 0, -- FIXME: This should be dynamic
-			espIV  = iv,
-		}
-	end)
-
-	local bufs = memory.bufArray()
-	while dpdk.running() do
-		local rx = rxQueue:recv(bufs)
-		local new_bufs = mem:bufArray(rx) -- allocate space for the new, encapsulated packets
-		new_bufs:alloc(pkt_len) -- FIXME: does this mean we have fixed pkt lengths only?
-		for i = 1, rx do
-			local buf  = bufs[i] -- original packet buffer
-			local new_buf = new_bufs[i] -- new packet buffer for encapsulated packet
-			local ip_pkt = buf:getIPPacket() -- original IP packet
-			local esp_pkt = new_buf:getEspPacket() -- new ESP packet
-
-			buf:dump(128) -- TODO: DEBUG
-			local ip_header = ip_pkt:getHeader()
-			local len = ip_header:getLengt()
-			-- TODO: put original packet into ESP packet
-
-			-- add 20 byte trailer, next_hdr = IPv4(0x04)/IPv6(0x29)
-			-- TODO: adopt length to the original packet's length
-			ipsec.add_esp_trailer(buf, 16, 0x04)
-		end
-		bufs:freeAll() -- free original packet buffer
-		new_bufs:offloadIPChecksums()
-		new_bufs:offloadIPSec(0, "esp", 1) -- enable hw IPSec in ESP/Encrypted mode, with SA/Key at index 0
-		txQueue:send(new_bufs)
-	end
-
-	-- Disable hw crypto engine
-	ipsec.disable(txPort)
-end
-
