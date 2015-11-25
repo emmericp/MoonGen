14fd1a1218e950c2bcfa83cbd7a6445182666d6e
Katsulynx <katsulynx@gmail.com>
2015-11-25 02:12:07 +0100

    "send"-Prototype

--- test/tests/send.lua
+++ test/tests/send.lua
@@ -4,12 +4,6 @@ local luaunit   = require('luaunit')
 local dpdk		= require "dpdk" -- TODO: rename dpdk module to "moongen"
 local memory	= require "memory"
 local device	= require "device"
-local ts		= require "timestamping"
-local filter	= require "filter"
-local stats		= require "stats"
-local hist		= require "histogram"
-local timer		= require "timer"
-local log		= require "log"
 
 local PKT_SIZE  = 60 -- without CRC
 local ETH_DST	= "10:11:12:13:14:15" -- src mac is taken from the NIC
@@ -17,8 +11,6 @@ local IP_SRC	= "192.168.0.1"
 local NUM_FLOWS	= 256 -- src ip will be IP_SRC + random(0, NUM_FLOWS - 1)
 local IP_DST	= "10.0.0.1"
 local PORT_SRC	= 1234
-local PORT_FG	= 42
-local PORT_BG	= 43
 
     function master()
         local txPort = 13;
@@ -30,44 +22,37 @@ local PORT_BG	= 43
         device.waitForLinks()
         dpdk.launchLua("slave", txDev, rxDev, txDev:getTxQueue(0), rate, PKT_SIZE)
         dpdk.waitForSlaves()
-        assertEquals(1,2)
     end
 
     function slave(queue, port)
-        mg.sleepMillis(100) -- wait a few milliseconds to ensure that the rx thread is running
-        -- TODO: implement barriers
+        mg.sleepMillis(100)
         local mem = memory.createMemPool(function(buf)
             buf:getUdpPacket():fill{
-                pktLength = PKT_SIZE, -- this sets all length headers fields in all used protocols
-                ethSrc = queue, -- get the src mac from the device
+                pktLength = PKT_SIZE,
+                ethSrc = queue,
                 ethDst = ETH_DST,
-                -- ipSrc will be set later as it varies
                 ip4Dst = IP_DST,
                 udpSrc = PORT_SRC,
-                udpDst = port,
-                -- payload will be initialized to 0x00 as new memory pools are initially empty
+                udpDst = port
             }
         end)
-        -- TODO: fix per-queue stats counters to use the statistics registers here
+    
         local txCtr = stats:newManualTxCounter("Port " .. port, "plain")
         local baseIP = parseIPAddress(IP_SRC)
-        -- a buf array is essentially a very thing wrapper around a rte_mbuf*[], i.e. an array of pointers to packet buffers
         local bufs = mem:bufArray()
+    
         while mg.running() do
-            -- allocate buffers from the mem pool and store them in this array
             bufs:alloc(PKT_SIZE)
+        
             for _, buf in ipairs(bufs) do
-                -- modify some fields here
                 local pkt = buf:getUdpPacket()
-                -- select a randomized source IP address
-                -- you can also use a wrapping counter instead of random
                 pkt.ip4.src:set(baseIP + math.random(NUM_FLOWS) - 1)
-                -- you can modify other fields here (e.g. different source ports or destination addresses)
             end
-            -- send packets
+        
             bufs:offloadUdpChecksums()
             txCtr:updateWithSize(queue:send(bufs), PKT_SIZE)
         end
+    
         txCtr:finalize()
     end
 
