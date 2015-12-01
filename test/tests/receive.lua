aee1fa291ae609872d6834d86b251543e306b697
Katsulynx <katsulynx@gmail.com>
2015-12-01 14:29:29 +0100

    "receive" proto

--- test/tests/receive.lua
+++ test/tests/receive.lua
@@ -15,12 +15,12 @@ TestSend = {}
 
     function master()
         local testPairs = tconfig.pairs()
-        local testPorts = {}
     
+        local testPorts = {}
         for i = 1, #testPairs do
             testPorts[i*2-1] = testPairs[i][1]
             testPorts[i*2] = testPairs[i][2]
-	end
+		end
     
         local testDevs = {}
 		for i, v in ipairs(testPorts) do
@@ -31,11 +31,13 @@ TestSend = {}
 		for i = 1, #testDevs, 2 do
 			TestSend["testNic" .. testDevs[i]] = function()
 				sendSlave( testDevs[i], testDevs[i+1] )
-                		luaunit.assertTrue( receiveSlave( testDevs[i+1] ) )
-            
-				sendSlave( testDevs[i+1], testDevs[i] )
-                		luaunit.assertTrue( receiveSlave( testDevs[i] ) )
+				luaunit.assertTrue( receiveSlave( testDevs[i+1] ) )
 			end
+        
+			TestSend["testNic" .. testDevs[i+1]] = function()
+				sendSlave( testDevs[i+1], testDevs[i] )
+				luaunit.assertTrue( receiveSlave( testDevs[i] ) )
+            end
 		end
 		os.exit( luaunit.LuaUnit.run() )
     end
