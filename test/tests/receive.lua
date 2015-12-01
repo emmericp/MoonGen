47cbd09eea011bd1be3b5d09e7e151e8d0a9c511
Katsulynx <ga46xuw@mytum.de>
2015-12-01 15:03:20 +0100

    "receive" proto

--- test/tests/receive.lua
+++ test/tests/receive.lua
@@ -18,28 +18,25 @@ TestSend = {}
     
         local testPorts = {}
         for i = 1, #testPairs do
-            testPorts[i*2-1] = testPairs[i][1]
-            testPorts[i*2] = testPairs[i][2]
-		end
+        	testPorts[i*2-1] = testPairs[i][1]
+        	testPorts[i*2] = testPairs[i][2]
+	end
     
         local testDevs = {}
-		for i, v in ipairs(testPorts) do
-			testDevs[i] = device.config{ port = testPorts[i], rxQueues = 2, txQueues = 3}
-		end
+	for i, v in ipairs(testPorts) do
+		testDevs[i] = device.config{ port = testPorts[i], rxQueues = 2, txQueues = 3}
+	end
         device.waitForLinks()
 
-		for i = 1, #testDevs, 2 do
-			TestSend["testNic" .. testDevs[i]] = function()
-				sendSlave( testDevs[i], testDevs[i+1] )
-				luaunit.assertTrue( receiveSlave( testDevs[i+1] ) )
-			end
-        
-			TestSend["testNic" .. testDevs[i+1]] = function()
-				sendSlave( testDevs[i+1], testDevs[i] )
-				luaunit.assertTrue( receiveSlave( testDevs[i] ) )
-            end
-		end
-		os.exit( luaunit.LuaUnit.run() )
+	for i = 1, #testDevs, 2 do
+		TestSend["testNic" .. testPorts[i] .. " " .. testPorts[i+1]] = function()
+			sendSlave( testDevs[i], testDevs[i+1] )
+			luaunit.assertTrue( receiveSlave( testDevs[i+1] ) )
+			sendSlave( testDevs[i+1], testDevs[i] )
+			luaunit.assertTrue( receiveSlave( testDevs[i] ) )
+           	 end
+	end
+	os.exit( luaunit.LuaUnit.run() )
     end
 
     function sendSlave(dev, target)
