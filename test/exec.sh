84580f5b20f2695d0288da297616c1f58d9bc274
Katsulynx <ga46xuw@mytum.de>
2015-12-02 03:30:28 +0100

    "exec.sh" updated

--- test/exec.sh
+++ test/exec.sh
@@ -1,2 +1,26 @@
-../build/MoonGen tests/send.lua
-../build/MoonGen tests/receive.lua
+#-------------------------
+#-Variables for later use-
+#-------------------------
+
+#Path to log file
+file='logs/log'$(date '+%Y-%m-%d:%H:%M:%S')'.txt'
+
+#Get test script list
+list=$(ls tests/*.lua)
+
+#Moongen Path
+path='../build/MoonGen'
+
+#----------------------------
+#-Execute Tests and evaluate-
+#----------------------------
+
+for script in $list
+do
+echo "Running $script"
+output=$(eval $path $script)
+echo "$output" >> $file
+echo "$output" >> 'temp.txt'
+eval= tail -2 temp.txt
+echo "$eval"
+done
