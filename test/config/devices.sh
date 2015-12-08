9d1434cacd3f61dd06a24ec2a96a3524d73e0fc1
Katsulynx <ga46xuw@mytum.de>
2015-12-08 13:55:38 +0100

    Autoconfig fetches devices from MoonGen and stores into temporary file

--- test/config/devices.sh
+++ test/config/devices.sh
@@ -1,6 +1,10 @@
+#--------------------------#
+#-Get devices from MoonGen-#
+#--Fecht output		 --#
+#--Strip devices out	 --#
+#--Format		 --#
+#--------------------------#
 output=$(../../build/MoonGen devices.lua)
 rm devices.txt
-echo $output > devices.txt
-output=$(sed -e 's/^.*Found/Found/p' devices.txt)
-rm devices.txt
-echo $output > devices.txt
+echo "$output" > devices.txt
+sed -n -E -i -e '/(.*Found.*)/,$ p' devices.txt | sed '1 d'
