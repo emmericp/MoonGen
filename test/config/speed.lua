2f03a39324157ed7929418a24af3b374e52a1a8d
Katsulynx <ga46xuw@mytum.de>
2015-12-08 16:50:42 +0100

    Autoconfig get device speed from moongen

---
+++ test/config/speed.lua
@@ -0,0 +1,19 @@
+local dpdk 	= require "dpdk"
+local device 	= require "device"
+package.path 	= package.path .. ";tconfig.lua"
+local tconfig 	= require "tconfig"
+
+
+function master()
+	local cards = tconfig.cards()
+	local devs = {}
+	for i=1, #cards do
+		devs[i] = device.config{ port = cards[i][1], rxQueues = 2, txQueues = 3}
+	end
+	device.waitForLinks()
+	
+	slave()
+end
+
+function slave()
+end
