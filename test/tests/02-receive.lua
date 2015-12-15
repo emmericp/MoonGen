b7cc3aedd832e6ac4a29482a9beb1d88564691db
Katsulynx <ga46xuw@mytum.de>
2015-12-15 14:04:03 +0100

    Reworking test cases

--- test/tests/02-receive.lua
+++ test/tests/02-receive.lua
@@ -8,12 +8,14 @@ local timer 	= require "timer"
 
 local tconfig   = dofile("config/tconfig.lua")
 
-local PKT_SIZE  = 60 -- without CRC
+local PKT_SIZE  = 20 -- without CRC
 
 TestSend = {}
 
 function master()
 	local pairs = tconfig.pairs()
+	local cards = tconfig.cards()
+
 	local ports = {}
         for i = 1, #pairs do
         	ports[i*2-1] = pairs[i][1]
@@ -28,10 +30,10 @@ function master()
 
 	for i = 1, #devs, 2 do
 		TestSend["testNic" .. ports[i] .. " " .. ports[i+1]] = function()
-			local packages = sendSlave( devs[i], devs[i+1] )
-			luaunit.assertTrue( receiveSlave( testDevs[i+1], packages ) )
-			packages = sendSlave( devs[i+1], devs[i] )
-			luaunit.assertTrue( receiveSlave( testDevs[i], packages ) )
+			local packages = sendSlave( devs[i], cards[tonumber(ports[i+1])+1][2] )
+			luaunit.assertTrue ( receiveSlave( devs[i+1], packages ) )
+			packages = sendSlave( devs[i+1], cards[tonumber(ports[i])+1][2] )
+			luaunit.assertTrue( receiveSlave( devs[i], packages ) )
 		end
 	end
 	os.exit( luaunit.LuaUnit.run() )
@@ -39,26 +41,30 @@ function master()
 
 function sendSlave(dev, target)
         local queue = dev:getTxQueue(0)
-        local tqueue = target:getTxQueue(0)
         dpdk.sleepMillis(100)
-    
+
+
+	print(target)    
         local mem = memory.createMemPool(function(buf)
             buf:getEthernetPacket():fill{
                 pktLength = PKT_SIZE,
-                ethSrc = queue, --random src
-                ethDst = tqueue, --random dst
+                ethSrc = queue,
+                ethDst = target
             }
         end)
     
         local bufs = mem:bufArray()
-        local runtime = timer:new(10)
+        local max = 1000
+	local runtime = timer:new(0.1)
 	local i = 0
-        while runtime:running() and dpdk.running() do
+
+        while runtime:running() and dpdk.running() and i < max do
         	bufs:alloc(PKT_SIZE)
        		queue:send(bufs)
 		i = i + 1
         end
- 
+
+	print(i)
         return i
 end
 
@@ -66,10 +72,19 @@ function receiveSlave(dev, packages)
         print("Testing Receive Capability: ", dev)
 	
 	local bufs = memory.bufArray()    
-        local queue = dev:getRxQueue(1)
-	local runtime = timer:new(10)
+        local queue = dev:getRxQueue(0)
+	local runtime = timer:new(1)
+
+	local received = 0
+	maxwait = 10
         while runtime:running() and dpdk.running() do
-            queue:tryRecv(bufs, 100)
+        	local rx = queue:tryRecv(bufs, maxWait)
+		print("TEST")
+		for i=1, rx do
+			local buf = bufs[i]
+			local pkt = buf:getEthernetPacket()
+			print(pkt)
+		end
         end
 	
         return 1 -- Test Successful
