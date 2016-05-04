43ccbac9430e555633d5820e89d954bf6c4c711f
Dominik Scholz <scholzd@in.tum.de>
2016-05-04 09:56:24 +0000

    [kni] Added kni_init

--- lua/include/kni.lua
+++ lua/include/kni.lua
@@ -1,6 +1,7 @@
 local ffi = require "ffi"
 local dpdkc = require "dpdkc"
 local dpdk = require "dpdk"
+require "utils"
 
 local mod = {}
 
@@ -23,11 +24,12 @@ int rte_kni_handle_request 	( 	struct rte_kni *  	kni	);
 unsigned mg_kni_tx_single(struct rte_kni * kni, struct rte_mbuf * mbuf);
 void rte_kni_close 	( 	void  		);
 int rte_kni_release 	( 	struct rte_kni *  	kni	);
+void rte_kni_init(unsigned int max_kni_ifaces);
 ]]
 
 function mod.createKNI(core, device, mempool, name)
   --printf("kni C pointer print:")
-  printPtr(mempool)
+  --printPtr(mempool)
   core = core or 0
   --printf("port id %d", device.id)
   --printf("in KNI ptr memp %p", mempool)
@@ -50,15 +52,21 @@ end
 function mg_kni:rxBurst(bufs, nmax)
   return ffi.C.rte_kni_rx_burst(self.kni, bufs.array, nmax)
 end
+
 function mg_kni:txSingle(mbuf)
   ffi.C.mg_kni_tx_single(self.kni, mbuf)
 end
+
 function mg_kni:handleRequest()
   ffi.C.rte_kni_handle_request(self.kni)
 end
 
 function mg_kni:release()
   return ffi.C.rte_kni_release(self.kni)
+end
+
+function mod.init(num)
+  return ffi.C.rte_kni_init(num)
 end
 
 function mod.close()
