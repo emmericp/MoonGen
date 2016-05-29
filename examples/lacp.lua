112681cc44cb9be9873a885ac46d96b1a4ea1d8f
Paul Emmerich <p.emmerich@first-colo.net>
2016-05-29 10:05:29 +0000

    use UDP traffic

--- examples/lacp.lua
+++ examples/lacp.lua
@@ -7,8 +7,13 @@ local stats		= require "stats"
 local hist		= require "histogram"
 local lacp		= require "proto.lacp"
 
-local PKT_SIZE = 60
-local ETH_DST = "ff:ff:ff:ff:ff:ff"
+local PKT_SIZE      = 60
+local ETH_DST       = "90:E2:BA:C0:EE:8C"
+local SRC_IP        = "10.10.10.1"
+local DST_IP        = "10.10.10.2"
+local SRC_PORT_BASE = 3000 -- + NIC id 
+local DST_PORT_BASE = 4000 -- + count(NUM_FLOWS)
+local NUM_FLOWS		= 1 -- per port
 
 function master(...)
 	if select("#", ...) < 2 then
@@ -38,16 +43,26 @@ end
 
 function loadSlave(queue)
 	local mem = memory.createMemPool(function(buf)
-		buf:getEthernetPacket():fill{
+		buf:getUdpPacket():fill{
 			ethSrc = queue,
 			ethDst = ETH_DST,
-			ethType = 0x1234
+			ip4Src = SRC_IP,
+			ip4Dst = DST_IP,
+			udpSrc = SRC_PORT_BASE + queue.id,
+			pktLength = PKT_SIZE
 		}
 	end)
 	local bufs = mem:bufArray()
 	local txCtr = stats:newDevTxCounter(queue.dev, "plain")
+	local counter = 0
 	while dpdk.running() do
 		bufs:alloc(PKT_SIZE)
+		for i, buf in ipairs(bufs) do
+			local pkt = buf:getUdpPacket()
+			pkt.udp:setDstPort(DST_PORT_BASE + counter)
+			counter = incAndWrap(counter, NUM_FLOWS)
+		end
+		bufs:offloadUdpChecksums()
 		queue:send(bufs)
 		txCtr:update()
 	end
