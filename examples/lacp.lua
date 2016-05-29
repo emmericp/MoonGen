d2c34df610d95ce3cdf7e4726c8937381168a3be
Paul Emmerich <p.emmerich@first-colo.net>
2016-05-29 11:03:55 +0000

    lacp: remove timestamper
    the timestamping logic unfortunately doesn't handle multiple
    possible RX interfaces yet; not yet sure how to handle this
    properly

--- examples/lacp.lua
+++ examples/lacp.lua
@@ -25,19 +25,18 @@ function master(...)
 	rate = rate or 10000
 	local lacpQueues = {}
 	for i = 1, select("#", ...) - 1 do
-		local port = device.config{port = ports[i], rxQueues = 4, txQueues = 4} 
-		lacpQueues[#lacpQueues + 1] = { rx = port:getRxQueue(1), tx = port:getTxQueue(1) }
+		local port = device.config{port = ports[i], rxQueues = 3, txQueues = 3} 
+		lacpQueues[#lacpQueues + 1] = {rx = port:getRxQueue(1), tx = port:getTxQueue(1)}
 		ports[i] = port
 	end
 	device.waitForLinks()
-	dpdk.launchLua(lacp.lacpTask, { name = "bond0", ports = lacpQueues})
+	dpdk.launchLua(lacp.lacpTask, {name = "bond0", ports = lacpQueues})
 	lacp.waitForLink("bond0")
 	for i, port in ipairs(ports) do 
 		local queue = port:getTxQueue(0)
 		queue:setRate(rate)
 		dpdk.launchLua("loadSlave", queue)
 	end
-	--dpdk.launchLua("timerSlave", txDev:getTxQueue(0), rxDev:getRxQueue(1), histfile)
 	dpdk.waitForSlaves()
 end
 
@@ -68,16 +67,5 @@ function loadSlave(queue)
 		txCtr:update()
 	end
 	txCtr:finalize()
-end
-
-function timerSlave(txQueue, rxQueue, histfile)
-	local timestamper = ts:newTimestamper(txQueue, rxQueue)
-	local hist = hist:new()
-	dpdk.sleepMillis(1000) -- ensure that the load task is running
-	while dpdk.running() do
-		hist:update(timestamper:measureLatency(function(buf) buf:getEthernetPacket().eth.dst:setString(ETH_DST) end))
-	end
-	hist:print()
-	hist:save(histfile)
 end
 
