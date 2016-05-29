59b07eeecc8dd318a9041a3959b94550dd862347
Paul Emmerich <p.emmerich@first-colo.net>
2016-05-29 11:57:01 +0000

    add slightly incorrect timestamping with lacp

--- examples/lacp.lua
+++ examples/lacp.lua
@@ -24,35 +24,44 @@ function master(...)
 	ports[#ports] = nil
 	rate = rate or 10000
 	local lacpQueues = {}
+	local pingQueues = {}
 	for i = 1, select("#", ...) - 1 do
 		local port = device.config{port = ports[i], rxQueues = 3, txQueues = 3} 
 		lacpQueues[#lacpQueues + 1] = {rx = port:getRxQueue(1), tx = port:getTxQueue(1)}
+		pingQueues[#pingQueues + 1] = {rx = port:getRxQueue(2), tx = port:getTxQueue(2)}
 		ports[i] = port
 	end
 	device.waitForLinks()
 	dpdk.launchLua(lacp.lacpTask, {name = "bond0", ports = lacpQueues})
 	lacp.waitForLink("bond0")
+	local lacpSource = lacp.getMac("bond0")
 	for i, port in ipairs(ports) do 
 		local queue = port:getTxQueue(0)
 		queue:setRate(rate)
-		dpdk.launchLua("loadSlave", queue)
+		dpdk.launchLua("loadSlave", queue, lacpSource)
 	end
+	--dpdk.launchLua("timerSlave", pingQueues, lacpSource)
 	dpdk.waitForSlaves()
 end
 
-function loadSlave(queue)
+local function fillPacket(buf, srcMac, qid, size)
+	buf:getUdpPacket():fill{
+		ethSrc = srcMac,
+		ethDst = ETH_DST,
+		ip4Src = SRC_IP,
+		ip4Dst = DST_IP,
+		udpSrc = SRC_PORT_BASE + qid,
+		pktLength = size or PKT_SIZE
+	}
+end
+
+function loadSlave(queue, lacpSource)
 	local mem = memory.createMemPool(function(buf)
-		buf:getUdpPacket():fill{
-			ethSrc = queue,
-			ethDst = ETH_DST,
-			ip4Src = SRC_IP,
-			ip4Dst = DST_IP,
-			udpSrc = SRC_PORT_BASE + queue.id,
-			pktLength = PKT_SIZE
-		}
+		fillPacket(buf, lacpSource, queue.id)
 	end)
 	local bufs = mem:bufArray()
 	local txCtr = stats:newDevTxCounter(queue.dev, "plain")
+	local rxCtr = stats:newDevRxCounter(queue.dev, "plain")
 	local counter = 0
 	while dpdk.running() do
 		bufs:alloc(PKT_SIZE)
@@ -65,7 +74,36 @@ function loadSlave(queue)
 		--bufs:setVlans(1234)
 		queue:send(bufs)
 		txCtr:update()
+		rxCtr:update()
 	end
 	txCtr:finalize()
+	rxCtr:finalize()
+end
+
+-- FIXME: this obviously loses all pakets that arrive on the wrong lacp member
+function timerSlave(queues, lacpSource)
+	local timestampers = {}
+	for i, queue in ipairs(queues) do
+		timestampers[#timestampers + 1] = ts:newUdpTimestamper(queue.tx, queue.rx)
+	end
+	local hist = hist:new()
+	dpdk.sleepMillis(1000) -- ensure that the load task is running
+	local size = math.max(84, PKT_SIZE)
+	local counter = 0
+	while dpdk.running() do
+		for i, queue in ipairs(queues) do
+			local timestamper = timestampers[i]
+			local lat = timestamper:measureLatency(size, function(buf)
+				fillPacket(buf, lacpSource, queue.tx.id, size)
+				local pkt = buf:getUdpPacket()
+				pkt.udp:setSrcPort(DST_PORT_BASE + counter)
+				pkt.ip4.dst:set(math.random(0, 2^32 - 1))--setDstPort(DST_PORT_BASE + counter)
+				counter = incAndWrap(counter, NUM_FLOWS)
+			end)
+		--	hist:print()
+		end
+	end
+	hist:print()
+	hist:save(histfile)
 end
 
