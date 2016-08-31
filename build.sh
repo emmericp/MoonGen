74f41b4690ae6681239492d62ebf32653bf73dcf
Paul Emmerich <p.emmerich@first-colo.net>
2016-08-31 13:51:31 +0200

    detect number of CPUs for parallel build

--- build.sh
+++ build.sh
@@ -4,21 +4,24 @@
 cd $(dirname "${BASH_SOURCE[0]}")
 git submodule update --init --recursive
 
+
+NUM_CPUS=$(cat /proc/cpuinfo  | grep "processor\\s: " | wc -l)
+
 (
 cd phobos/deps/luajit
-make -j 16 BUILDMODE=static 'CFLAGS=-DLUAJIT_NUMMODE=2 -DLUAJIT_ENABLE_LUA52COMPAT'
+make -j $NUM_CPUS BUILDMODE=static 'CFLAGS=-DLUAJIT_NUMMODE=2 -DLUAJIT_ENABLE_LUA52COMPAT'
 make install DESTDIR=$(pwd)
 )
 
 (
 cd phobos/deps/dpdk
-make -j 16 install T=x86_64-native-linuxapp-gcc
+make -j $NUM_CPUS install T=x86_64-native-linuxapp-gcc
 )
 
 (
 cd build
 cmake ..
-make -j 16
+make -j $NUM_CPUS
 )
 
 echo Trying to bind interfaces, this will fail if you are not root
