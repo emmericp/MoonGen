11f685ff8f3a46ff82fd5c9b3ff2f5c9ad7a78ba
Artem Shvorin <art@highloadlab.com>
2016-09-22 13:53:04 +0300

    rx-to-pcap test: no default value for --sink, just skip writing pcap

--- examples/pcap/rx-to-pcap.lua
+++ examples/pcap/rx-to-pcap.lua
@@ -13,7 +13,7 @@ local stats		= require "stats"
 function configure(parser)
 	parser:description("Reads packets from device and writes them to pcap files.")
 	parser:argument("dev", "Devices to receive from."):args("+"):convert(tonumber)
-	parser:option("-s --sink", "File prefix to write pcap data: 'PREFIX-portNum-queueNum.pcap'; default is 'pcap-port-'."):default("pcap-port-")
+	parser:option("-s --sink", "File prefix to write pcap data: 'PREFIX-portNum-queueNum.pcap'")
 	parser:option("-q --queues", "Number of RX queues."):default(1):convert(tonumber)
 	parser:option("-m --maxp", "Reads at most maxp packets or forever if maxp == 0."):default(0):convert(tonumber)
 
@@ -22,11 +22,11 @@ end
 function master(args)
 	for i, dev in ipairs(args.dev) do
 		local rxDev = device.config{ port = dev, rxQueues = args.queues }
-		args.sink = args.sink or 'pcap-port-'
-		args.maxp = args.maxp / args.queues
+		local maxp = args.maxp / args.queues
 		rxDev:wait()
 		for i = 0, args.queues-1 do
-			mg.startTask("pcapSinkSlave", rxDev:getRxQueue(i), rxDev, args.sink..dev..'-'..i..'.pcap', args.maxp)
+			local sink = args.sink and args.sink..dev..'-'..i..'.pcap'
+			mg.startTask("pcapSinkSlave", rxDev:getRxQueue(i), sink, maxp)
 		end
 	end
 
@@ -36,18 +36,18 @@ end
 
 
 --! @brief: receive and store packets with software timestamps
-function pcapSinkSlave(queue, rxDev, sink, maxp)
+function pcapSinkSlave(queue, sink, maxp)
 	print('pcapSinkSlave is running')
 	local numbufs = (maxp == 0) and 100 or math.min(100, maxp)
 	local bufs = memory.bufArray(numbufs)
 
-	local pcapSinkWriter = pcapWriter:newPcapWriter(sink)
-	local ctr = stats:newDevRxCounter(rxDev, "plain")
+	local pcapSinkWriter = sink and pcapWriter:newPcapWriter(sink)
+	local ctr = stats:newDevRxCounter(queue, "plain")
 	local pkts = 0
 	while mg.running() and (maxp == 0 or pkts < maxp) do
 		local rxnum = (maxp == 0) and #bufs or math.min(#bufs, maxp - pkts)
 		local rx = queue:recvWithTimestamps(bufs, rxnum)
-		if rx > 0 then
+		if pcapSinkWriter and rx > 0 then
 			pcapSinkWriter:writeTSC(bufs, bufs, rx, true)
 		end
 		pkts = pkts + rx
@@ -58,5 +58,7 @@ function pcapSinkSlave(queue, rxDev, sink, maxp)
 	bufs:freeAll()
 
 	ctr:finalize()
-	pcapSinkWriter:close()
+	if pcapSinkWriter then
+		pcapSinkWriter:close()
+	end
 end
