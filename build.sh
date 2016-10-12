14a1781b83a95a06a8c66df843816c3868a86900
Paul Emmerich <emmericp@net.in.tum.de>
2016-10-12 21:57:28 +0200

    phobos is now libmoon
    please rerun build.sh after applying this commit

--- build.sh
+++ build.sh
@@ -8,13 +8,13 @@ git submodule update --init --recursive
 NUM_CPUS=$(cat /proc/cpuinfo  | grep "processor\\s: " | wc -l)
 
 (
-cd phobos/deps/luajit
+cd libmoon/deps/luajit
 make -j $NUM_CPUS BUILDMODE=static 'CFLAGS=-DLUAJIT_NUMMODE=2 -DLUAJIT_ENABLE_LUA52COMPAT'
 make install DESTDIR=$(pwd)
 )
 
 (
-cd phobos/deps/dpdk
+cd libmoon/deps/dpdk
 make -j $NUM_CPUS install T=x86_64-native-linuxapp-gcc
 )
 
