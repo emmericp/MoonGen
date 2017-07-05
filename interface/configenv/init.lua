15bc07a7db1358150a48c780e91652dfdf0582a3
Daniel <Reisz@users.noreply.github.com>
2017-07-05 08:38:40 +0200

    Add safe Lua globals to config.

--- interface/configenv/init.lua
+++ interface/configenv/init.lua
@@ -1,6 +1,17 @@
-return function(...)
-  require "configenv.setup" (...)
-  require "configenv.range" (...)
-  require "configenv.util" (...)
-  return ...
+local _safe_methods = {}
+for v in string.gmatch([[
+  string table math
+  getmetatable
+  ipairs next pairs
+  rawequal rawget rawlen
+  tonumber tostring type
+]], "%S+") do
+  safeMethods[v] = _G[v]
+end
+
+return function(tbl, ...)
+  require "configenv.setup" (tbl, ...)
+  require "configenv.range" (tbl, ...)
+  require "configenv.util" (tbl, ...)
+  return setmetatable(tbl, { __index = _safe_methods }), ...
 end
