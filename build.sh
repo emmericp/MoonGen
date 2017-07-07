4247c4fc425f98c12bb8c323b7e4eef5c72f65c3
Alexander Frank <ap.frank@tum.de>
2017-07-07 13:21:10 +0200

    Enable user defined build options

--- build.sh
+++ build.sh
@@ -1,5 +1,35 @@
 #!/bin/bash
 
+OPTIONS=''
+MLX5=false
+MLX4=false
+
+while :; do
+        case $1 in
+                -h|--help)
+                        echo "Usage: <no option> compile without Mellanox drivers; <-m|--mlx5> compile mlx5; <-n|--mlx4> compile mlx4; <-h|--help> help"
+                        exit
+                        ;;
+                -m|--mlx5)
+                        echo "Build with mlx5 driver selected"
+                        OPTIONS="$OPTIONS""-DUSE_MLX5=ON "
+                        MLX5=true
+                        ;;
+                -n|--mlx4)
+                        echo "Build with mlx4 driver selected"
+                        OPTIONS="$OPTIONS""-DUSE_MLX4=ON "
+                        MLX4=true
+                        ;;
+                -?*)
+                        printf 'WARN: Unknown option (abort): %s\n' "$1" >&2
+                        exit
+                        ;;
+                *)
+                        break
+        esac
+        shift
+done
+
 (
 cd $(dirname "${BASH_SOURCE[0]}")
 git submodule update --init --recursive
@@ -15,12 +45,20 @@ make install DESTDIR=$(pwd)
 
 (
 cd libmoon/deps/dpdk
-make -j $NUM_CPUS install T=x86_64-native-linuxapp-gcc
+#build DPDK with the right configuration
+make config T=x86_64-native-linuxapp-gcc O=x86_64-native-linuxapp-gcc
+if ${MLX5} ; then
+        sed -ri 's,(MLX5_PMD=).*,\1y,' x86_64-native-linuxapp-gcc/.config
+fi
+if ${MLX4} ; then
+        sed -ri 's,(MLX4_PMD=).*,\1y,' x86_64-native-linuxapp-gcc/.config
+fi
+make -j $NUM_CPUS O=x86_64-native-linuxapp-gcc
 )
 
 (
 cd build
-cmake ..
+cmake ${OPTIONS}..
 make -j $NUM_CPUS
 )
 
