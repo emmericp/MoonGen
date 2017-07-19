f1d19034eb283e99bf07aa823ae8c473ede6b131
Reisz <leinad.ffafp@gmail.com>
2017-07-19 22:18:43 +0200

    Improve flow error messages.

--- interface/configenv/setup.lua
+++ interface/configenv/setup.lua
@@ -8,8 +8,8 @@ local function _init_tbl(tbl, key)
 end
 
 local function _copy_packet(src, dest)
-	if dest.proto then
-		assert(dest.proto == src.proto) -- TODO error message
+	if dest.proto and dest.proto ~= src.proto then
+		return false, "Cannot inherit from flow with different packet protocol."
 	else
 		dest.proto = src.proto
 	end
@@ -24,14 +24,21 @@ local function _copy_packet(src, dest)
 	for _,v in ipairs(src.dynvars) do
 		table.insert(tbl, v)
 	end
+
+	return true
 end
 
 return function(env, error, flows)
+
 	function env.Flow(tbl)
 		local name = tbl[1]
-		error.assert(not string.find(name, ","), "Invalid character ',' in flow name.")
-		error.assert(not string.find(name, ":"), "Invalid character ':' in flow name.")
-		error.assert(not string.find(name, " "), "Invalid character ' ' in flow name.")
+
+		-- check for characters that
+		-- - would complicate shell argument parsing ( ;)
+		-- - interfere with flow parameter syntax (:,)
+		if string.find(name, "[ ;:,]") then
+			error("Invalid flow name %q. Names cannot include the characters ' ;:,'.", name)
+		end
 
 		local flow = {
 			packet = {}
@@ -40,7 +47,7 @@ return function(env, error, flows)
 		if tbl.parent then
 			for i,v in pairs(flows[tbl.parent]) do
 				if i == "packet" then
-					_copy_packet(v, flow.packet)
+					error:assert(_copy_packet(v, flow.packet))
 				else
 					flow[i] = v
 				end
@@ -48,7 +55,7 @@ return function(env, error, flows)
 		end
 
 		flow.name = name
-		_copy_packet(tbl[2], flow.packet)
+		error:assert(_copy_packet(tbl[2], flow.packet))
 
 		flows[name] = flow
 	end
@@ -65,15 +72,19 @@ return function(env, error, flows)
 				for i,v in pairs(tbl) do
 					local pkt, var = string.match(i, "^([%l%d]+)(%u[%l%d]*)$");
 
-					if type(v) == "function" then
-						var = string.lower(var)
-						table.insert(packet.dynvars, {
-							pkt = pkt, var = var, func = v
-						})
-						v = v() -- NOTE arp will execute in master
-					end
+					if pkt then
+						if type(v) == "function" then
+							var = string.lower(var)
+							table.insert(packet.dynvars, {
+								pkt = pkt, var = var, func = v
+							})
+							v = v() -- NOTE arp will execute in master
+						end
 
-					packet.fillTbl[i] = v
+						packet.fillTbl[i] = v
+					else
+						error("Invalid packet field %q.", i) -- TODO add hint?
+					end
 				end
 				return packet
 			end
