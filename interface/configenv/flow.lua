764b158d65b368882d9aebd680fbf55bd9b34453
Reisz <leinad.ffafp@gmail.com>
2017-07-26 16:14:13 +0200

    Make flow options self-contained.

--- interface/configenv/flow.lua
+++ interface/configenv/flow.lua
@@ -1,28 +1,5 @@
 local Flow = {}
 
-local _option_list = "rate"
-
-function Flow.new(name, tbl)
-	local parent = tbl.parent
-	local self = {
-		name = name,
-		-- TODO figure out actual queue requirements
-		tx_txq = 1, tx_rxq = 1, rx_txq = 1, rx_rxq = 1,
-		packet = tbl[2]:inherit(parent and parent.packet)
-	}
-
-	if parent then
-		self.parent = parent.name
-		-- NOTE add copy opertations here
-	end
-	return setmetatable(self, { __index = Flow })
-end
-
-function Flow:validate(val)
-	self.packet:validate(val)
-	-- TODO more validation
-end
-
 local _time_units = {
 	[""] = 1, ms = 1 / 1000, s = 1, m = 60, h = 3600
 }
@@ -32,34 +9,74 @@ local _size_units = {
 	m = 10 ^ 6, mi = 2 ^ 20,
 	g = 10 ^ 9, gi = 2 ^ 30
 }
-local function _parse_rate(self)
-	local num, unit, time = string.match(self.options.rate, "^(%d+%.?%d*)(%a*)/?(%a*)$")
-	num, unit, time = tonumber(num), string.lower(unit), _time_units[time]
 
-	if unit == "" then
-		unit = _size_units.m * 8
-	elseif string.find(unit, "bit$") then
-		unit = _size_units[string.sub(unit, 1, -4)]
-	elseif string.find(unit, "b$") then
-		unit = _size_units[string.sub(unit, 1, -2)] * 8
-	elseif string.find(unit, "p$") then
-		unit = _size_units[string.sub(unit, 1, -2)] * self.packet:size()
+local _option_list = {
+	rate = {
+		parse = function(self, rate)
+			if type(rate) == "number" then
+				self.cbr = rate
+				return
+			end
+
+			local num, unit, time = string.match(rate, "^(%d+%.?%d*)(%a*)/?(%a*)$")
+			num, unit, time = tonumber(num), string.lower(unit), _time_units[time]
+
+			if unit == "" then
+				unit = _size_units.m * 8
+			elseif string.find(unit, "bit$") then
+				unit = _size_units[string.sub(unit, 1, -4)]
+			elseif string.find(unit, "b$") then
+				unit = _size_units[string.sub(unit, 1, -2)] * 8
+			elseif string.find(unit, "p$") then
+				unit = _size_units[string.sub(unit, 1, -2)] * self.packet:size()
+			end
+
+			unit = unit / 10 ^ 6 -- cbr is in mbit/s
+			self.cbr = num * unit / time
+		end,
+		validate = function(val, rate)
+			if type(rate) ~= "number" then
+				val:assert(string.match(rate, "^(%d+%.?%d*)(%a*)/?(%a*)$"),
+					"Invalid value for option 'rate.'")
+			end
+		end
+	}
+}
+
+function Flow.new(name, tbl)
+	local parent = tbl.parent
+
+	local self = {
+		name = name, parent = parent,
+		-- TODO figure out actual queue requirements
+		tx_txq = 1, tx_rxq = 1, rx_txq = 1, rx_rxq = 1,
+		packet = tbl[2]:inherit(parent and parent.packet)
+	}
+
+	for i in pairs(_option_list) do
+		self[i] = tbl[i] or (parent and parent[i])
 	end
 
-	unit = unit / 10 ^ 6 -- cbr is in mbit/s
-	self.cbr = num * unit / time
+	return setmetatable(self, { __index = Flow })
+end
+
+function Flow:validate(val)
+	self.packet:validate(val)
+
+	for name, ops in pairs(_option_list) do
+		local v = self.options[name] or self[name]
+		if v then
+			ops.validate(val, v)
+		end
+	end
 end
 
 function Flow:prepare()
-	-- update options
-	for opt in string.gmatch(_option_list, "%w+") do
-		if not self.options[opt] then
-			self.options[opt] = self[opt]
+	for name, ops in pairs(_option_list) do
+		local v = self.options[name] or self[name]
+		if v then
+			ops.parse(self, v)
 		end
-	end
-
-	if self.options.rate then
-		_parse_rate(self)
 	end
 end
 
