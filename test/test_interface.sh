a91baad57078cf98e40533979eead8fb082c972d
Reisz <leinad.ffafp@gmail.com>
2017-09-13 11:25:51 +0200

    Fix error test script.

--- test/test_interface.sh
+++ test/test_interface.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-sudo ./build/MoonGen interface/init.lua -c test/flows f1:1:1 valid:100:100 valid:a:b valid:1:1,test=1,rate=a
+echo -e "Testing error management system. No flow should be able to start.\n"
+exec sudo ./build/MoonGen interface/init.lua send -c test/flows f1:1:1:test=1,rate=a valid:100:100 valid:a:b valid:1:1
