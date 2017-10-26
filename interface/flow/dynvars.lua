32b3258a85545b5fe451ea4eadfdd9156f31a552
Reisz <leinad.ffafp@gmail.com>
2017-10-26 11:31:29 +0200

    Fix aliases.

--- interface/flow/dynvars.lua
+++ interface/flow/dynvars.lua
@@ -4,13 +4,13 @@ local dynvar = {}
 dynvar.__index = dynvar
 
 local _aliases = {
-	udpSrc = proto.udp.setSrcPort, udpDst = proto.udp.setDstPort,
-	tcpSrc = proto.tcp.setSrcPort, tcpDst = proto.tcp.setDstPort,
-	ethSrc = proto.eth.default.setSrc, ethDst = proto.eth.default.setDst,
-	ethVlan = proto.eth.vlan.setVlanTag,
-	ethinnerVlanTag = proto.eth.qinq.setInnerVlanTag,
-	ethouterVlanId = proto.eth.qinq.setOuterVlanTag,
-	ethouterVlanTag = proto.eth.qinq.setOuterVlanTag,
+	udpSrc = proto.udp.metatype.setSrcPort, udpDst = proto.udp.metatype.setDstPort,
+	tcpSrc = proto.tcp.metatype.setSrcPort, tcpDst = proto.tcp.metatype.setDstPort,
+	ethSrc = proto.eth.default.metatype.setSrc, ethDst = proto.eth.default.metatype.setDst,
+	ethVlan = proto.eth.vlan.metatype.setVlanTag,
+	ethinnerVlanTag = proto.eth.qinq.metatype.setInnerVlanTag,
+	ethouterVlanId = proto.eth.qinq.metatype.setOuterVlanTag,
+	ethouterVlanTag = proto.eth.qinq.metatype.setOuterVlanTag,
 }
 local function _find_setter(pkt, var)
 	local alias = _aliases[pkt .. var]
