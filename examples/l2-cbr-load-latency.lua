acedbd8cab38f6d57523077a7779f20f66f96b37
brenton <brenton.d.walker@gmail.com>
2017-11-24 13:37:31 +0100

    change l2-cbr-load-latency.lua to take rate in Mbps (wire) and add a -s packet size argument

--- examples/l2-cbr-load-latency.lua
+++ examples/l2-cbr-load-latency.lua
@@ -14,11 +14,14 @@ function configure(parser)
 	parser:argument("txDev", "Device to transmit from."):args(1):convert(tonumber)
 	parser:argument("rxDev", "Device to receive from."):args(1):convert(tonumber)
 	parser:option("-r --rate", "Transmit rate in Mpps."):args(1):default(2)
+	parser:option("-s --size", "Packet size to use (min=60, max~~1500)"):args(1):default(60)
 end
 
 function master(args)
 	local txDev = device.config({port = args.txDev, txQueues = 2, rxQueues = 2})
 	local rxDev = device.config({port = args.rxDev, txQueues = 2, rxQueues = 2})
+	PKT_SIZE = math.max(60, tonumber(args.size))
+	print("using packet size "..PKT_SIZE)
 	device.waitForLinks()
 	mg.startTask("loadSlave", txDev, rxDev, txDev:getTxQueue(0), args.rate, PKT_SIZE)
 	mg.startTask("timerSlave", txDev:getTxQueue(1), rxDev:getRxQueue(1), PKT_SIZE)
@@ -38,8 +41,16 @@ function loadSlave(dev, rxDev, queue, rate, size)
 		bufs:alloc(size)
 		for _, buf in ipairs(bufs) do
 			-- this script uses Mpps instead of Mbit (like the other scripts)
-			buf:setDelay((10^10 / 8 / (rate * 10^6) - size - 24))
-			--buf:setRate(rate)
+			--buf:setDelay((10^10 / 8 / (rate * 10^6) - size - 24))
+			buf:setDelay((size+24) * (1000/rate - 1) )
+			--buf:setRate(rate*10)  -- rate in Mpps on gigabit ethernet
+			-- from crc-ratecontrol.lua:
+			--   delay The time to wait before this packet \(in bytes, i.e. 1 == 0.8 nanoseconds on 10 GbE\)
+			--   self.udata64 = 10^10 / 8 / (rate * 10^6) - self.pkt_len - 24
+			-- key code from stats.lua (accounting for preamble + inter-packet gap = 8+12?): 
+			-- 	local mpps = (pkts - self.total) / elapsed / 10^6
+			-- 	local mbit = (bytes - self.totalBytes) / elapsed / 10^6 * 8
+			-- 	local wireRate = mbit + (mpps * 20 * 8)
 		end
 		txStats:updateWithSize(queue:sendWithDelay(bufs), size)
 		rxStats:update()
