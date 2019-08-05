834aed0d980e8388b979d28eccaa5e61bb7d6058
Paul Emmerich <emmericp@net.in.tum.de>
2019-08-05 17:28:33 +0200

    replay-pcap: auto-stop if not looping

--- examples/pcap/replay-pcap.lua
+++ examples/pcap/replay-pcap.lua
@@ -12,6 +12,7 @@ function configure(parser)
 	parser:argument("dev", "Device to use."):args(1):convert(tonumber)
 	parser:argument("file", "File to replay."):args(1)
 	parser:option("-r --rate-multiplier", "Speed up or slow down replay, 1 = use intervals from file, default = replay as fast as possible"):default(0):convert(tonumber):target("rateMultiplier")
+	parser:option("-s --buffer-flush-time", "Time to wait before stopping MoonGen after enqueuing all packets. Increase for pcaps with a very low rate."):default(10):convert(tonumber):target("bufferFlushTime")
 	parser:flag("-l --loop", "Repeat pcap file.")
 	local args = parser:parse()
 	return args
@@ -24,12 +25,14 @@ function master(args)
 	if args.rateMultiplier > 0 then
 		rateLimiter = limiter:new(dev:getTxQueue(0), "custom")
 	end
-	mg.startTask("replay", dev:getTxQueue(0), args.file, args.loop, rateLimiter, args.rateMultiplier)
+	local replayer = mg.startTask("replay", dev:getTxQueue(0), args.file, args.loop, rateLimiter, args.rateMultiplier, args.bufferFlushTime)
 	stats.startStatsTask{txDevices = {dev}}
+	replayer:wait()
+	mg:stop()
 	mg.waitForTasks()
 end
 
-function replay(queue, file, loop, rateLimiter, multiplier)
+function replay(queue, file, loop, rateLimiter, multiplier, sleepTime)
 	local mempool = memory:createMemPool(4096)
 	local bufs = mempool:bufArray()
 	local pcapFile = pcap:newReader(file)
@@ -69,5 +72,7 @@ function replay(queue, file, loop, rateLimiter, multiplier)
 			queue:sendN(bufs, n)
 		end
 	end
+	log:info("Enqueued all packets, waiting for %d seconds for queues to flush", sleepTime)
+	mg.sleepMillisIdle(sleepTime * 1000)
 end
 
